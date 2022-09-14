4b7585007303771de19a605137d1f4a380f5944a
MinseoPark <kjms0820@gmail.com>
2022-09-15 00:14:31 +0900

    [BOJ] 균형잡힌 세상 / Silver 4 / 51M / 스택

--- 균형잡힌 세상.py
+++ 균형잡힌 세상.py
@@ -12,18 +12,28 @@ while True:
         for i in range(len(s)):
             if s[i] == '(' or s[i] == '[':
                 stack.append(s[i])
-            else:
+            # 괄호 말고 다른 문자열도 있기 때문에 괄호 조건을 넣어줘야 함
+            elif s[i] == ')':
                 if len(stack) == 0:
                     answer = False
-                elif stack[-1] == '(' and s[i] == ')':
+                    break
+                elif stack[-1] == '(':
                     stack.pop()
-                elif stack[-1] == '[' and s[i] == ']':
+                else:
+                    answer = False
+                    break
+            elif s[i] == ']':
+                if len(stack) == 0:
+                    answer = False
+                    break
+                elif stack[-1] == '[':
                     stack.pop()
+                else:
+                    answer = False
+                    break
 
         if len(stack) != 0:
             answer = False
-        else:
-            answer = True
 
 
         if answer:
