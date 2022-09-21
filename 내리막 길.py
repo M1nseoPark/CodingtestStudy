109e230ed5ffe48e28963da1ae10a7acd468cfd9
MinseoPark <kjms0820@gmail.com>
2022-09-21 23:16:06 +0900

    [BOJ] 내리막 길 / Gold 3 / X / DFS + DP / *

---
+++ 내리막 길.py
@@ -0,0 +1,39 @@
+# 전형적인 DP 문제
+# 동적계획법 공부하고 다시 풀어보기
+
+import sys
+sys.setrecursionlimit(10000)
+
+
+m, n = map(int, input().split())
+height = []
+for _ in range(m):
+    height.append(list(map(int, input().split())))
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+def dfs(y, x):
+    if y == (m - 1) and x == (n - 1):
+        return 1
+
+    if visited[y][x] != -1:
+        return visited[y][x]
+
+    visited[y][x] = 0
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if 0 <= nx < n and 0 <= ny < m and height[y][x] > height[ny][nx]:
+            visited[y][x] += dfs(ny, nx)
+
+    return visited[y][x]
+
+
+visited = [[-1] * n for _ in range(m)]
+print(dfs(0, 0))
+            
+        
+    
