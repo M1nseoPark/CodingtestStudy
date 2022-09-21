a050e2f2d075e67edce6f73d95bab10c3bfc9306
MinseoPark <kjms0820@gmail.com>
2022-09-22 00:08:08 +0900

    [BOJ] 트리의 부모 찾기 / Silver 2 / X / DFS

---
+++ 트리의 부모 찾기.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(1000000)
+
+n = int(input())
+node = [[] for _ in range(n+1)]   # 인접 행렬로 풀면 메모리 초과
+
+for i in range(n-1):
+    a, b = map(int, input().split())
+    node[a].append(b)   
+    node[b].append(a)
+
+
+def dfs(v):
+    for i in node[v]:
+        if parents[i] == 0:
+            parents[i] = v
+            dfs(i)
+
+
+parents = [0] * (n + 1) 
+dfs(1)
+
+for i in range(2, n+1):
+    print(parents[i])
