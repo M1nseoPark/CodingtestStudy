3bc0224de385ed206475b478055e48285ca5fc61
MinseoPark <kjms0820@gmail.com>
2022-09-21 17:36:33 +0900

    [BOJ] 적록색약 / Gold 5 / X / DFS / **

--- 적록색약.py
+++ 적록색약.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(10000)
+
 n = int(input())
 picture = []
 for _ in range(n):
@@ -6,92 +9,41 @@ for _ in range(n):
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
+visited = [[0] * n for _ in range(n)]   # picture이 0과 1이 아니므로 편의상
+
 def dfs(x, y):
-    if x < 0 or x >= n or y < 0 or y >= n:
-        return False
+    visited[x][y] = 1
 
-    if picture[x][y] == 'R':
-        picture[x][y] = 'C'
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            rdfs(nx, ny)
+        if nx >= 0 and nx < n and ny >= 0 and ny < n and visited[nx][ny] == 0 and picture[x][y] == picture[nx][ny]:
+            dfs(nx, ny)
+            
 
-        return True
-    
-    else:
-        return False
-
-
-def gdfs(x, y):
-    if x < 0 or x >= n or y < 0 or y >= n:
-        return False
-
-    if picture[x][y] == 'G':
-        picture[x][y] = 'C'
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            gdfs(nx, ny)
-
-        return True
-    
-    else:
-        return False
-
-
-def bdfs(x, y):
-    if x < 0 or x >= n or y < 0 or y >= n:
-        return False
-
-    if picture[x][y] == 'B':
-        picture[x][y] = 'X'
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            bdfs(nx, ny)
-
-        return True
-    
-    else:
-        return False
-
-
-def cdfs(x, y):
-    if x < 0 or x >= n or y < 0 or y >= n:
-        return False
-
-    if picture[x][y] == 'C':
-        picture[x][y] = 'X'
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            cdfs(nx, ny)
-
-        return True
-    
-    else:
-        return False
-
-area = [0] * 4
+answer1 = 0
+answer2 = 0
 for i in range(n):
     for j in range(n):
-        if rdfs(i, j):
-            area[0] += 1
-        if gdfs(i, j):
-            area[1] += 1
-        if bdfs(i, j):
-            area[2] += 1
-        if cdfs(i, j):
-            area[3] += 1
+        if visited[i][j] == 0:
+            answer1 += 1
+            dfs(i, j)
 
+# 적록색약 구현
 for i in range(n):
-    print(picture[i])
-print(area)
+    for j in range(n):
+        if picture[i][j] == 'R':
+            picture[i][j] = 'G'
+
+visited = [[0] * n for _ in range(n)]
+for i in range(n):
+    for j in range(n):
+        if visited[i][j] == 0:
+            answer2 += 1
+            dfs(i, j)
+
+print(answer1, answer2)
         
 
         
