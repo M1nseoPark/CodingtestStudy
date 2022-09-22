85030ef7181b56db1817db9a6db45e68c92382be
MinseoPark <kjms0820@gmail.com>
2022-09-22 22:50:33 +0900

    [BOJ] 피보나치 함수 / Silver 3 / X / DP / *

---
+++ 피보나치 함수.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+zero = [1, 0, 1]
+one = [0, 1, 1]
+
+def fibonacci(n):
+    if len(zero) <= n:   # 이미 계산해둔 수보다 클 때만 계산함
+        for i in range(len(zero), n+1):
+            zero.append(zero[i-1] + zero[i-2])
+            one.append(one[i-1] + one[i-2])
+
+    print(zero[n], one[n])
+
+
+for _ in range(t):
+    n = int(input())    
+    fibonacci(n)
+
+    
