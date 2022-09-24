2eef0a4e59cd1c3613ae2ed1de9423c770415efa
MinseoPark <kjms0820@gmail.com>
2022-09-24 23:16:58 +0900

    [BOJ] 트리의 부모 찾기 / Silver 2 / X / DFS / *

--- SOLVE.py
+++ SOLVE.py
@@ -2,36 +2,27 @@ import sys
 sys.setrecursionlimit(10000)
 
 n = int(input())
-a, b = map(int, input().split())
-m = int(input())
-family = [[0] * (n + 1) for _ in range(n+1)]
+tree = [[] for _ in range(n+1)]
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    tree[a].append(b)
 
-for _ in range(m):
-    x, y = map(int, input().split())
-    family[x][y] = 1
-    family[y][x] = 1
-
+parent = [0] * (n + 1)
 visited = [False for _ in range(n+1)]
 
-def dfs(v, t):
-    t += 1
+
+def dfs(v):
     visited[v] = True
-
-    if v == b:
-        answer.append(t)
-        
-
+    
     for i in range(1, n+1):
-        if (not visited[i]) and family[v][i] == 1:
-            dfs(i, t)
+        parent[tree[i][0]] = v
+        visited[tree[i][0]] = True
+        dfs(i)
 
-answer = []
-dfs(a, 0)
-
-if len(answer) == 0:
-    print(-1)
-else:
-    print(answer[0] - 1)
+dfs(1)
+for i in range(2, n+1):
+    print(parent[i])
+    
     
 
     
