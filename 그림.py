9048eb8ada55d9f9d8d228044d4441603bc6cd7b
MinseoPark <kjms0820@gmail.com>
2022-09-25 17:26:36 +0900

    [BOJ] 그림 / Silver 1 / 5M / BFS

--- 그림.py
+++ 그림.py
@@ -1,38 +1,36 @@
-# DFS로는 안풀리는 문제인듯
-
-import sys
-sys.setrecursionlimit(1000000)
-
 n, m = map(int, input().split())
 draw = []
-
-for i in range(n):
+for _ in range(n):
     draw.append(list(map(int, input().split())))
 
 dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
+dy = [-1, 1, 0, 0]
 
-def dfs(y, x):
+def bfs(y, x):
+    global temp
+    q = []
+    q.append([y, x])
     draw[y][x] = 0
-    global area
-    area += 1
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+    while q:
+        y, x = q.pop(0)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-        if 0 <= nx < m and 0 <= ny < n and draw[ny][nx] == 1:
-            dfs(ny, nx)
+            if 0 <= nx < m and 0 <= ny < n and draw[ny][nx] == 1:
+                q.append([ny, nx])
+                draw[ny][nx] = 0
+                temp += 1
 
-
-area = 0
+temp = 1
 answer = []
 for i in range(n):
     for j in range(m):
         if draw[i][j] == 1:
-            dfs(i, j)
-            answer.append(area)
-            area = 0
+            bfs(i, j)
+            answer.append(temp)
+            temp = 1
 
 if len(answer) == 0:
     print(0)
