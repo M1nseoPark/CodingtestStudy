944d6a86ee806487c355285f834f12c92b6d30f4
MinseoPark <kjms0820@gmail.com>
2022-09-25 16:46:41 +0900

    [BOJ] 토마토 / Gold 5 / X / BFS / *

--- SOLVE.py
+++ SOLVE.py
@@ -1,20 +1,40 @@
-n = int(input())
-seat = input()
+m, n = map(int, input().split())
+box = []
+for _ in range(n):
+    box.append(list(map(int, input().split())))
 
-s = 1
-i = 0
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-while i < n:
-    if seat[i] == 'S':
-        s += 1
 
-    if seat[i] == 'L':
-        s += 1
-        i += 1
+def bfs(y, x):
+    q = []
+    q.append([y, x])
 
-    i+= 1
+    while q:
+        y, x = q.pop(0)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n and box[ny][nx] == 0:
+                box[ny][nx] = 1
+                q.append([ny, nx])
 
-print(min(s, n))
+answer = 0
+for i in range(n):
+    for j in range(m):
+        if box[i][j] == 1:
+            bfs(i, j)
+            answer += 1
+
+print(answer)
+print(box)
+            
+            
+
+
+
+
         
     
     
