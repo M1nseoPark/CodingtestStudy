de9e974ba895ab585f0597a45c01ca6db0d42f2f
MinseoPark <kjms0820@gmail.com>
2022-09-26 15:43:23 +0900

    [BOJ] 숨바꼭질 / Silver 1 / X / BFS

---
+++ 숨바꼭질.py
@@ -0,0 +1,21 @@
+n, k = map(int, input().split())
+
+visited = [0 for _ in range(10**5+1)]   # 최대는 2*k가 아니라 100000
+def bfs(v):
+    q = []
+    q.append(v)
+
+    while q:
+        here = q.pop(0)
+
+        if here == k:
+            break
+
+        # for문 이렇게 쓰는 거 빼고 다 맞았는데 아깝.. 
+        for nv in (here - 1, here + 1, here * 2):
+            if 0 <= nv <= 10 ** 5 and visited[nv] == 0:
+                q.append(nv)
+                visited[nv] = visited[here] + 1
+
+bfs(n)
+print(visited[k])
