55a0e2faeb8b5d79d2458d47e7d694704bd19e2b
MinseoPark <kjms0820@gmail.com>
2022-09-27 22:13:43 +0900

    [BOJ] 치킨 배달 / Gold 5 / 1H 3M / 완전탐색

--- 치킨 배달.py
+++ 치킨 배달.py
@@ -1,9 +1,16 @@
+# 치킨집의 개수 최대 M개 -> 개수가 정해지지 않았는데 코드를 어떻게 짜지?
+# 조합으로 풀기엔 너무 경우의 수가 많아질 것 같음..
+## 진짜 combinations을 썼다고??? -> 파이썬 한정 쉬운 문제인듯
+
+from itertools import combinations
+
+
 n, m = map(int, input().split())
 city = []
 for _ in range(n):
     city.append(list(map(int, input().split())))
 
-# 좌표 -1
+
 house = []
 chicken = []
 for i in range(n):
@@ -14,41 +21,27 @@ for i in range(n):
         if city[i][j] == 2:
             chicken.append([i, j])
 
-
-distance = {}
-result = []
-for i in range(len(chicken)):
-    t = []
-    for j in range(len(house)):
-        t.append(abs(chicken[i][0] - house[j][0]) + abs(chicken[i][1] - house[j][1]))
-
-    distance[i] = t
-    result.append([i, sum(distance[i])])
-
-result.sort(key=lambda x:(x[1], x[0]), reverse=True)
-print(distance)
-print(result)
-
+num = 1
 answer = []
-while len(distance) > 0:
-    num = 0
+while num <= m:
+    pick = list(combinations(chicken, num))
+    
+    for k in range(len(pick)):
+        result = 0
+        for i in range(len(house)):
+            temp = []
+            for j in range(num):
+                temp.append(abs(pick[k][j][0] - house[i][0]) + abs(pick[k][j][1] - house[i][1]))
 
-    for i in range(len(distance[0])):
-        temp = 2*n
-        k = distance.pop(result[0][0])
-        for j in range(len(distance)):
-            if distance[j][i] < temp:
-                temp = distance[j][i]
-                
-        num += temp
+            result += min(temp)
             
-    if len(distance) <= m:
-        answer.append(num)
-        
-    del distance[result[0][0]]
-    result.pop(0)
+        answer.append(result)
 
-print(min(answer))
+    num += 1
+
+print(min(answer))    
+
+
     
 
 
