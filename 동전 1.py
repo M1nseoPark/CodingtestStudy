f57d0676c46c546f3995b19536380b6b0872f246
MinseoPark <kjms0820@gmail.com>
2022-09-27 23:02:18 +0900

    [BOJ] 동전 1 / Gold 5 / X / DP / #

---
+++ 동전 1.py
@@ -0,0 +1,16 @@
+# 모르겠어요... 이해가 안돼요...
+
+'''
+    1   2   3   4   5   6   7   8   9   10
+1   1   1   1   1   1   1   1   1   1   1
+2   0   1   1   1   
+5
+
+
+'''
+
+n, k = map(int, input().split())
+coin = []
+for _ in range(n):
+    coin.append(int(input()))
+
