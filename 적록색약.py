82b4dd42afe3624280500ec2ab46c4efdd81a407
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2022-10-02 14:57:29 +0900

    [BOJ] 적록색약 / Gold 5 / 11M / BFS

--- 적록색약.py
+++ 적록색약.py
@@ -1,6 +1,3 @@
-import sys
-sys.setrecursionlimit(10000)
-
 n = int(input())
 picture = []
 for _ in range(n):
@@ -9,42 +6,43 @@ for _ in range(n):
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-visited = [[0] * n for _ in range(n)]   # picture이 0과 1이 아니므로 편의상
+visited = [[0] * n for _ in range(n)]
 
-def dfs(x, y):
-    visited[x][y] = 1
+def bfs(y, x, c):
+    q = []
+    q.append([y, x])
+    visited[y][x] = 1
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+    while q:
+        y, x = q.pop()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and visited[ny][nx] == 0 and picture[ny][nx] == c:
+                visited[ny][nx] = 1
+                q.append([ny, nx])
 
-        if nx >= 0 and nx < n and ny >= 0 and ny < n and visited[nx][ny] == 0 and picture[x][y] == picture[nx][ny]:  ##
-            dfs(nx, ny)
-            
 
-answer1 = 0
-answer2 = 0
+normal = 0
 for i in range(n):
     for j in range(n):
         if visited[i][j] == 0:
-            answer1 += 1
-            dfs(i, j)
+            bfs(i, j, picture[i][j])
+            normal += 1
 
-# 적록색약 구현
+
 for i in range(n):
     for j in range(n):
-        if picture[i][j] == 'R':
-            picture[i][j] = 'G'
+        if picture[i][j] == 'G':
+            picture[i][j] = 'R'
 
 visited = [[0] * n for _ in range(n)]
+blind = 0
 for i in range(n):
     for j in range(n):
         if visited[i][j] == 0:
-            answer2 += 1
-            dfs(i, j)
+            bfs(i, j, picture[i][j])
+            blind += 1
 
-print(answer1, answer2)
+print(normal, blind)
         
-
-        
-
