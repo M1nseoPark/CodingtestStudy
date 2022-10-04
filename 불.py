4a17536cbeff004a9d527e1d9b12c68570aa95d7
M1nseoPark <kjms0820@gmail.com>
2022-10-05 00:07:36 +0900

    [BOJ] 불 / Gold 4 / 30M / BFS

--- 불.py
+++ 불.py
@@ -1,40 +1,34 @@
-test = int(input())
+# 디큐 안쓰면 시간초과..
+
+import sys
+from collections import deque
+
+test = int(sys.stdin.readline())
 
 dx = [0, 0, -1, 1]
 dy = [-1, 1, 0, 0]
 
-def fbfs(y, x):
-    q = []
-    q.append([y, x])
-    fvisited[y][x] = 1
-    visited[y][x] = 1
-
-    while q:
-        y, x = q.pop(0)
+def fbfs():
+    while fq:
+        y, x = fq.popleft()
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < w and 0 <= ny < h and maps[ny][nx] == '.' and fvisited[ny][nx] == 0:
+            if 0 <= nx < w and 0 <= ny < h and maps[ny][nx] != '#' and fvisited[ny][nx] == 0:
                 fvisited[ny][nx] = fvisited[y][x] + 1
-
-                if visited[ny][nx] != 0:
-                    visited[ny][nx] = min(fvisited[ny][nx], visited[ny][nx])
-                else:
-                    visited[ny][nx] = fvisited[ny][nx]
-
-                q.append([ny, nx])
+                fq.append([ny, nx])
 
 
 def sbfs(y, x):
-    q = []
+    q = deque()
     q.append([y, x])
     svisited[y][x] = 1
     result = 0
 
     while q:
-        y, x = q.pop(0)
+        y, x = q.popleft()
 
         if y == 0 or y == (h - 1) or x == 0 or x == (w - 1):
             result = svisited[y][x]
@@ -44,8 +38,8 @@ def sbfs(y, x):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < w and 0 <= ny < h and maps[ny][nx] == '.':
-                if svisited[ny][nx] == 0 and (visited[ny][nx] == 0 or visited[ny][nx] > (svisited[y][x] + 1)):
+            if 0 <= nx < w and 0 <= ny < h and maps[ny][nx] != '#':
+                if svisited[ny][nx] == 0 and (fvisited[ny][nx] == 0 or fvisited[ny][nx] > (svisited[y][x] + 1)):
                     svisited[ny][nx] = svisited[y][x] + 1
                     q.append([ny, nx])
 
@@ -54,32 +48,31 @@ def sbfs(y, x):
     
 
 for _ in range(test):
-    w, h = map(int, input().split())
+    w, h = map(int, sys.stdin.readline().split())
     maps = []
+    fq = deque()
     answer = 0
 
-    visited = [[0] * w for _ in range(h)]
+    fvisited = [[0] * w for _ in range(h)]
     svisited = [[0] * w for _ in range(h)]
     
     for i in range(h):
-        maps.append(list(input()))
+        maps.append(list(sys.stdin.readline().rstrip()))
 
     for i in range(h):
         for j in range(w):
             if maps[i][j] == '*':
-                fvisited = [[0] * w for _ in range(h)]
-                fbfs(i, j)
+                fq.append([i, j])
+                fvisited[i][j] = 1
+
+    fbfs()
 
     for i in range(h):
         for j in range(w):
             if maps[i][j] == '@':
                 answer = sbfs(i, j)
+                break
 
-    for i in range(h):
-        print(visited[i])
-    print('-----')
-    for i in range(h):
-        print(svisited[i])
 
     if answer != 0:
         print(answer)
