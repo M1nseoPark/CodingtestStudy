2e3e08a8e0585be60a81b87b467b87c6e5c6bb2e
M1nseoPark <kjms0820@gmail.com>
2022-10-09 12:48:55 +0900

    [BOJ] 뱀 / Gold 4 / X / 시뮬레이션

--- 뱀_삼성.py
+++ 뱀_삼성.py
@@ -1,4 +1,3 @@
-### 맞왜틀!!!!
 ### 거의 다 맞게 풀었는데 아깝다..
 ### 큐는 굳이 2개 만들 필요 없었음
 ### BFS라고 꼭 while q 쓰는 거 아님! 잘 생각해보고 코드 짜기
@@ -9,10 +8,10 @@ n = int(input())
 k = int(input())
 #### 인덱스는 0부터 시작하는게 구현하기 편함
 board = [[0] * n for _ in range(n)]  # 0행 0열부터 시작 (그래프를 모두 0으로 채워줌)
-# 사과 위치는 모두 1로 채워줌
+# 사과 위치는 모두 1로 채워줌 -> 이거때매 엄~~청 헤맴
 for _ in range(k):
     a, b = map(int, input().split())
-    board[b-1][a-1] = 1
+    board[a-1][b-1] = 1
 
 l = int(input())
 way = {}  #### 딕셔너리로 구현하면 좋음
@@ -60,9 +59,6 @@ def bfs():
         else:
             break
 
-        print(snake)
-        print(time, ny, nx)
-        print('------')
 
 bfs()
 print(time)
