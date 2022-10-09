cef70425168fca63303556098be2d8fa1884b7cb
MinseoPark <kjms0820@gmail.com>
2022-10-09 15:24:06 +0900

    [BOJ] 주사위 굴리기 / Gold 4 / X / 시뮬레이션

--- 주사위 굴리기_삼성.py
+++ 주사위 굴리기_삼성.py
@@ -1,16 +1,15 @@
 ## 주사위는 숫자 기준이 아니라 방향 기준 -> 동서남북으로 굴렸을 때 인덱스 변화 저장
-## 맞왜틀..
+## 놓여져 있는 곳의 좌표는 (x, y)이다 -> y, x 바꿔야함!!
 n, m, x, y, k = map(int, input().split())
 maps = []
 for _ in range(n):
-    a1, a2 = map(int, input().split())
-    maps.append([a1, a2])
+    maps.append(list(map(int, input().split())))
 
 dice = [0, 0, 0, 0, 0, 0]
 order = list(map(int, input().split()))
 
-dx = [1, -1, 0, 0]  # 동서북남
-dy = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]  # 동서북남
+dx = [0, 0, -1, 1]
 
 def turn(dir):
     a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
@@ -31,14 +30,14 @@ def roll(ny, nx, d):
     nx = nx + dx[d]
     ny = ny + dy[d]
     
-    if 0 <= nx < m and 0 <= ny < n:
+    if 0 <= nx < n and 0 <= ny < m:
         turn(d)
 
-        if maps[ny][nx] == 0:   # 0이면 주사위 바닥면에 쓰여 있는 수가 칸에 복사됨
-            maps[ny][nx] = dice[-1]
+        if maps[nx][ny] == 0:   # 0이면 주사위 바닥면에 쓰여 있는 수가 칸에 복사됨
+            maps[nx][ny] = dice[-1]
         else:   # 0이 아니면 칸에 쓰여 있는 수가 주사위 바닥면으로 복사되며, 칸에 쓰여 있는 수는 0이 됨
-            dice[-1] = maps[ny][nx]
-            maps[ny][nx] = 0
+            dice[-1] = maps[nx][ny]
+            maps[nx][ny] = 0
 
         print(dice[0])
         return ny, nx
