8c4e10543831df6a62e39dcd8f5f0eaa1308e176
M1nseoPark <kjms0820@gmail.com>
2022-10-10 23:47:51 +0900

    [BOJ] 주사위 굴리기2 / Gold 3 / 1H 30M / 시뮬레이션

--- 주사위 굴리기 2_삼성.py
+++ 주사위 굴리기 2_삼성.py
@@ -6,28 +6,29 @@ for _ in range(n):
     maps.append(list(map(int, input().split())))
 
 dice = [1, 2, 3, 4, 5, 6]
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]  # 동남서북 
+dx = [1, 0, -1, 0]
 
 def roll(dir):
     a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
     if dir == 0:  # 동
         dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, b, a, f, e, c
-    elif dir == 1:  # 서
-        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, a, e, d
-    elif dir == 2:  # 북
-        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, a, c, d, f, b
-    else:  # 남
+    elif dir == 1:  # 남
         dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = b, f, c, d, a, e
+    elif dir == 2:  # 서
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, a, e, d
+    else:  # 북
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, a, c, d, f, b
 
 
 # BFS로 풀면 되는데 왜 생각 못했지?
-def score(sy, sx, s):
+def score(y, x, s):
     count = 0
     q = deque()
-    q.append([sy, sx])
+    q.append([y, x])
     visited = [[0] * m for _ in range(n)]
-    visited[sy][sx] = 1
+    visited[y][x] = 1
+    
     while q:
         y, x = q.popleft()
         count += 1
@@ -45,6 +46,7 @@ answer = 0
 way = 0
 x, y = 0, 0
 for _ in range(k):
+    # 이동 방향으로 한 칸 굴러감
     if 0 <= y + dy[way] < n and 0 <= x + dx[way] < m:
         y += dy[way]
         x += dx[way]
@@ -55,6 +57,8 @@ for _ in range(k):
             way = (way + 1) % 4
         elif dice[-1] < maps[y][x]:
             way = (way - 1) % 4
+
+    # 이동 방향에 칸이 없다면, 이동 방향을 반대로 한 다음 한 칸 굴러감
     else:
         if way == 0 or way == 1:
             way += 2
