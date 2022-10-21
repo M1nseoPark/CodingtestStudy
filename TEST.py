659728b74bb84d74884ab88dd9f3ea12ed7618a2
M1nseoPark <kjms0820@gmail.com>
2022-10-21 21:57:53 +0900

    [BOJ] 큐 / Silver 4 / 10M / 큐

--- TEST.py
+++ TEST.py
@@ -1,1 +1,15 @@
-print(round(4.5))
+n = int(input())
+stair = []
+for _ in range(n):
+    stair.append(int(input()))
+
+dp = [0] * (n + 1)
+dp[1] = stair[1]
+dp[2] = stair[1] + stair[2]
+dp[3] = max(stair[1] + stair[3], stair[2] + stair[3])
+
+for i in range(4, n+1):
+    dp[i] = max(dp[i-3] + stair[i-3] + stair[i-1],
+                dp[i-3] + stair[i-2] + stair[i-1])
+
+print(dp[n])
