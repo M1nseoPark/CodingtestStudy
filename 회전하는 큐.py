52f091a355d07719b8ce4bc3df0cbb4670b036e6
M1nseoPark <kjms0820@gmail.com>
2022-10-22 22:06:31 +0900

    [BOJ] 회전하는 큐 / Silver 3 / X / 덱

--- 회전하는 큐.py
+++ 회전하는 큐.py
@@ -2,20 +2,21 @@ from collections import deque
 
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-deque = deque([i for i in range(1, n+1)])
 
+dq = deque([i for i in range(1, n+1)])
 ans = 0
+
 for i in arr:
     while True:
-        if num[0] == i:
-            deque.popleft()
+        if dq[0] == i:
+            dq.popleft()
             break
 
-        if deque.index(i) <= (n - idx):
-            data.append(data.pop(0))
+        if dq.index(i) <= len(dq)//2:
+            dq.append(dq.popleft())
             ans += 1
         else:
-            data = [data[-1]] + data[:-1]
+            dq.appendleft(dq.pop())
             ans += 1
 
 print(ans)
