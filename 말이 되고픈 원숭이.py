a02ac062d17dd7f9f745188699e32ebccb48a748
MinseoPark <kjms0820@gmail.com>
2022-10-27 12:35:08 +0900

    [BOJ] 말이 되고픈 원숭이 / Gold 3 / X / BFS

--- 말이 되고픈 원숭이.py
+++ 말이 되고픈 원숭이.py
@@ -1,3 +1,5 @@
+# 벽 부수기 문제랑 비슷! -> 3차원 visited 리스트를 사용해야 함
+
 from collections import deque
 
 k = int(input())
@@ -7,45 +9,38 @@ board = []
 for _ in range(h):
     board.append(list(map(int, input().split())))
 
-dx = [-1, 1, -2, 2, -2, 2, -1, 1, -1, 1, 0, 0]
-dy = [-2, -2, -1, -1, 1, 1, 2, 2, 0, 0, -1, 1]
+dy = [-2, -2, -1, -1, 1, 1, 2, 2, -1, 1, 0, 0]
+dx = [-1, 1, -2, 2, -2, 2, -1, 1, 0, 0, -1, 1]
 
-def bfs(y, x):
-    q = deque()
-    q.append([y, x])
-    board[y][x] = 1
-    follow = 0
-
+def bfs():
     while q:
-        y, x = q.popleft()
+        y, x, z = q.popleft()
 
         if y == (h - 1) and x == (w - 1):
-            return move[y][x]
+            return visited[y][x][z]
 
-        if follow < k:
+        if z < k:
             for i in range(8):
                 nx = x + dx[i]
                 ny = y + dy[i]
+                if 0 <= nx < w and 0 <= ny < h:
+                    if board[ny][nx] == 0 and visited[ny][nx][z+1] == 0:
+                        q.append([ny, nx, z+1])
+                        visited[ny][nx][z+1] = visited[y][x][z] + 1
 
-                if 0 <= nx < w and 0 <= ny < h and board[ny][nx] == 0:
-                    q.append([ny, nx])
-                    board[ny][nx] = 1
-                    move[ny][nx] = move[y][x] + 1
-                    follow += 1
-                        
-            
         for i in range(8, 12):
             nx = x + dx[i]
             ny = y + dy[i]
-
-            if 0 <= nx < w and 0 <= ny < h and board[ny][nx] == 0:
-                q.append([ny, nx])
-                board[ny][nx] = 1
-                move[ny][nx] = move[y][x] + 1
+            if 0 <= nx < w and 0 <= ny < h:
+                if board[ny][nx] == 0 and visited[ny][nx][z] == 0:
+                    q.append([ny, nx, z])
+                    visited[ny][nx][z] = visited[y][x][z] + 1
 
     return -1
-
-
-move = [[0] * w for _ in range(h)]
-print(bfs(0, 0))
+                
+                                    
+q = deque()
+q.append([0, 0, 0])
+visited = [[[0] * 31 for _ in range(w)] for _ in range(h)]
+print(bfs())
     
