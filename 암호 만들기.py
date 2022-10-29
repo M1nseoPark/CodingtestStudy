0fe32e04e5b6a08d4163fb0173901fb016aef3ae
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2022-10-29 16:03:31 +0900

    [BOJ] 암호 만들기 / Gold 5 / 10M / 백트래킹

--- 암호 만들기.py
+++ 암호 만들기.py
@@ -1,39 +1,30 @@
 l, c = map(int, input().split())
-word = list(input().split())
+arr = list(input().split())
 
-answer = []
-def makeCode(code):
+code = []
+visited = [False for _ in range(c)]
+arr.sort()
+
+def pick(code):
     if len(code) == l:
-        vowel = 0
+        vo, co = 0, 0
+        for i in code:
+            if i in ['a', 'e', 'i', 'o', 'u']:
+                vo += 1
+            else:
+                co += 1
 
-        # 모음 개수 굳이 미리 세어줄 필요 없이 마지막에 세는게 편함
-        for i in range(l):
-            if code[i] in ['a', 'e', 'i', 'o', 'u']:
-                vowel += 1
+        if co >= 2 and vo >= 1:
+            print(''.join(code))
 
-        if vowel >= 1 and (l - vowel) >= 2:
-            answer.append(code)
-                
     else:
         for i in range(c):
-            if len(code) == 0:
-                code += word[i]
-                makeCode(code)
-                code = code[:-1]
-            # word가 정렬되었더라도 원소를 추가할 때는 조건이 필요함
-            elif word[i] not in code and word[i] > code[-1]:
-                code += word[i]
-                makeCode(code)
-                code = code[:-1]
+            if (not visited[i]) and (len(code) == 0 or code[-1] < arr[i]):
+                code.append(arr[i])
+                visited[i] = True
+                pick(code)
+                code.pop()
+                visited[i] = False
 
+pick(code)
 
-code = ''
-makeCode(code)
-answer.sort()
-
-for i in answer:
-    print(i)
-
-    
-
-    
