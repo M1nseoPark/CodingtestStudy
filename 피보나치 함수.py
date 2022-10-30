4ade17142c16e6adcf629e511e9c9f3db1fac113
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2022-10-30 15:05:55 +0900

    [BOJ] 피보나치 함수 / Silver 3 / 14M / DP

--- 피보나치 함수.py
+++ 피보나치 함수.py
@@ -1,19 +1,19 @@
 t = int(input())
 
-zero = [1, 0, 1]
-one = [0, 1, 1]
+one = [0] * 41
+zero = [0] * 41
+zero[0] = 1
+one[1] = 1
+
 
 def fibonacci(n):
-    if len(zero) <= n:   # 이미 계산해둔 수보다 클 때만 계산함
-        for i in range(len(zero), n+1):
-            zero.append(zero[i-1] + zero[i-2])
-            one.append(one[i-1] + one[i-2])
-
-    print(zero[n], one[n])
+    for i in range(2, n+1):
+        if one[i] == 0 or zero[i] == 0:
+            one[i] = one[i-1] + one[i-2]
+            zero[i] = zero[i-1] + zero[i-2]
 
 
 for _ in range(t):
-    n = int(input())    
+    n = int(input())
     fibonacci(n)
-
-  
+    print(zero[n], one[n])
