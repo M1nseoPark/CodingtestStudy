8443f455024f3919f0b9c9fb73e0421c686c61dd
M1nseoPark <kjms0820@gmail.com>
2022-10-31 22:17:25 +0900

    [BOJ] 쉬운 계단 수 / Silver 1 / X / DP

---
+++ 쉬운 계단 수.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+dp = [[0] * 10 for _ in range(n+1)]
+
+for i in range(1, 10):
+    dp[1][i] = 1
+
+for i in range(2, n+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][1]
+
+        elif j == 9:   # 9 뒤엔 오직 숫자 8만 올 수 있음
+            dp[i][j] = dp[i-1][8]
+
+        else:   # 1~8은 뒤에 올 수 있는 숫자가 총 2종류
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1] 
+
+print(sum(dp[n]) % 1000000000)
+    
