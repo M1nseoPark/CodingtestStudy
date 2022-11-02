707fc972197aacb9057304bb1b7ee3ea4461a2d6
M1nseoPark <kjms0820@gmail.com>
2022-11-02 17:33:49 +0900

    [BOJ] 카드 / Silver 4 / 20M / 정렬

--- 카드.py
+++ 카드.py
@@ -9,18 +9,9 @@ for _ in range(n):
     else:
         card[t] = 1
         
-card = sorted(card.items(), key=lambda x:(x[1], x[0]), reverse=True)
-ak, av = card[0][0], card[0][1]
+card = sorted(card.items(), key=lambda x:(x[1], -x[0]), reverse=True)
 
-if n >= 2:
-    for i in range(1, n):
-        if av == card[i][1]:
-            ak = card[i][0]
-            av = card[i][1]
-        else:
-            break
-
-print(ak)
+print(card[0][0])
     
     
 
