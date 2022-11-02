ce80730bc444047fdb5bea0948bc10e8895130e1
M1nseoPark <kjms0820@gmail.com>
2022-11-02 14:52:11 +0900

    [BOJ] 좌표 정렬하기 2 / Silver 5 / 3M / 정렬

---
+++ 좌표 정렬하기 2.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+arr.sort(key=lambda x:(x[1], x[0]))
+
+for i in range(n):
+    print(arr[i][0], arr[i][1])
