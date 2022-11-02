d6750e15ebd65ff50e94535baf1e7c42a12a733f
M1nseoPark <kjms0820@gmail.com>
2022-11-02 17:30:11 +0900

    [BOJ] 국영수 / Silver 4 / 7M / 정렬

---
+++ 국영수.py
@@ -0,0 +1,10 @@
+n = int(input())
+student = []
+for _ in range(n):
+    a, b, c, d = input().split()
+    student.append([a, int(b), int(c), int(d)])
+
+student.sort(key=lambda x:(-x[1], x[2], -x[3], x[0]))
+
+for i in range(n):
+    print(student[i][0])
