2f4dd53d1e439b18e045c552089bae31e0dd0e5c
M1nseoPark <kjms0820@gmail.com>
2022-11-02 14:26:04 +0900

    [BOJ] 수 정렬하기 4 / Silver 5 / 4M / 정렬

---
+++ 수 정렬하기 4.py
@@ -0,0 +1,10 @@
+import sys
+
+n = int(sys.stdin.readline())
+arr = []
+for _ in range(n):
+    arr.append(int(sys.stdin.readline()))
+
+arr.sort(reverse=True)
+for i in range(n):
+    print(arr[i])
