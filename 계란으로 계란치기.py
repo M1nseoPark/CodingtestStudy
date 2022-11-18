e2e11d40a8fe2c984397ecb604e7d5aa47d99842
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2022-11-18 15:24:03 +0900

    [BOJ] 계란으로 계란 치기 / Silver 1 / 32M / 백트래킹

--- 계란으로 계란치기.py
+++ 계란으로 계란치기.py
@@ -5,48 +5,42 @@ egg = []
 for _ in range(n):
     egg.append(list(map(int, sys.stdin.readline().split())))
 
+answer = 0
+
 def hit(idx):
     global answer
-
+    
     if idx == n:
         temp = 0
         for i in range(n):
             if egg[i][0] <= 0:
                 temp += 1
+
         answer = max(temp, answer)
         return
 
-    # 자기가 깨져있는 경우 다음 계란으로
-    if egg[idx][0] <= 0:
+
+    if egg[idx][0] > 0:
+        remain = False
+        for i in range(n):
+            if idx != i and egg[i][0] > 0:
+                remain = True
+                break
+        
+        if remain:        
+            for i in range(n):
+                if idx != i and egg[i][0] > 0:
+                    egg[idx][0] -= egg[i][1]
+                    egg[i][0] -= egg[idx][1]
+                    hit(idx + 1)
+                    egg[idx][0] += egg[i][1]
+                    egg[i][0] += egg[idx][1]
+        else:
+            hit(idx + 1)
+    else:
         hit(idx + 1)
-        return
 
-    # 자기 빼고 다 깨져있는 경우
-    broken = True
-    for i in range(n):
-        if idx == i:
-            continue
-        
-        if egg[i][0] > 0:
-            broken = False
-            break
 
-    if broken:
-        answer = max(n - 1, answer)
-        return
-    
-    for i in range(n):
-        if idx == i or egg[i][0] <= 0:
-            continue
-
-        egg[i][0] -= egg[idx][1]
-        egg[idx][0] -= egg[i][1]
-        hit(idx + 1)
-        egg[i][0] += egg[idx][1]
-        egg[idx][0] += egg[i][1]
-        
-        
-answer = 0
 hit(0)
 print(answer)
         
