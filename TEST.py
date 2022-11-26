0e35dae40c8a8fd2876b60266c04e75c863d81d3
M1nseoPark <kjms0820@gmail.com>
2022-11-26 09:35:42 +0900

    [BOJ] 피보나치 수의 확장 / Silver 3 / 10M / DP

--- TEST.py
+++ TEST.py
@@ -1,3 +1,24 @@
+n = int(input())
+
+if n == 0:
+    print(0)
+    print(0)
+    
+else:
+    m = abs(n)
+    dp = [0] * (m + 1)
+    dp[1] = 1
+
+    for i in range(2, m+1):
+        dp[i] = dp[i-1] % 1000000000 + dp[i-2] % 1000000000
+
+    if n < 0 and m % 2 == 0:
+        print(-1)
+    else:
+        print(1)
+
+    print(dp[m] % 1000000000)
+    
 
     
                 
--- 피보나치 수의 확장.py
+++ 피보나치 수의 확장.py
@@ -1,25 +1,23 @@
-# 메모리 초과
-# 음수일때는 나머지 연산을 어떻게 해야하는지
-## 음수일 때 피보나치 수열 = 0, 1, -1, 2, -3, 5, ...
-
 n = int(input())
 
 if n == 0:
     print(0)
     print(0)
+    
 else:
-    dp = [0 for _ in range(abs(n)+1)]
+    m = abs(n)
+    dp = [0] * (m + 1)
     dp[1] = 1
 
-    for i in range(2, abs(n)+1):
-        dp[i] = (dp[i-1] % 1000000000) + (dp[i-2] % 1000000000)
+    for i in range(2, m+1):
+        dp[i] = dp[i-1] % 1000000000 + dp[i-2] % 1000000000
 
-    if n < 0 and abs(n) % 2 == 0:
+    if n < 0 and m % 2 == 0:
         print(-1)
     else:
         print(1)
 
-    print(dp[abs(n)])
+    print(dp[m] % 1000000000)
     
         
     
