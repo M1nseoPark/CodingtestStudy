aed5d346859da189e203c063d10a495a93e4c21e
M1nseoPark <kjms0820@gmail.com>
2022-12-07 23:53:58 +0900

    backup

--- SOLVE.py
+++ SOLVE.py
@@ -1,46 +1,72 @@
-n = int(input())
-arr = list(map(int, input().split()))
+r, c, k = map(int, input().split())
+A = []
+for _ in range(3):
+    A.append(list(map(int, input().split())))
 
-arr.sort()
-result = 3000000000
-answer = [0, 0, 0]
+rc, cc = 3, 3   # 행의 크기, 열의 크기
+time = 0
+while True:
+    if A[r-1][c-1] == k:
+        print(time)
+        break
+    
+    time += 1
+    
+    if time == 100:
+        print(-1)
+        break
 
-for i in range(n-2):
-    a = arr[i]
+    # R 연산 수행 
+    if rc >= cc:
+        tc = 0
+        for i in range(rc):
+            temp = {}
+            for j in range(cc):
+                if A[i][j] != 0:
+                    if A[i][j] in temp:
+                        temp[A[i][j]] += 1
+                    else:
+                        temp[A[i][j]] = 1
 
-    left = i + 1
-    right = n - 1
-    while left < right:
-        temp = a + arr[left] + arr[right]
+            arr = list(zip(temp.keys(), temp.values()))
+            arr.sort(key=lambda x: (x[1], x[0]))
+            A[i] = list(sum(arr, ()))
+            tc = max(tc, len(A[i]))
 
-        if abs(temp) < result:
-            result = abs(temp)
-            answer[0], answer[1], answer[2] = arr[i], arr[left], arr[right]
+        for i in range(rc):
+            if len(A[i]) < tc:
+                A[i] += [0] * (tc - len(A[i]))
 
-        if temp == 0:
-            break
+        cc = tc
 
-        elif temp < 0:
-            left += 1
+    # C 연산 수행
+    else:
+        tr = 0
+        for i in range(cc):
+            temp = {}
+            for j in range(rc):
+                if A[j][i] != 0:
+                    if A[j][i] in temp:
+                        temp[A[j][i]] += 1
+                    else:
+                        temp[A[j][i]] = 1
 
-        else:
-            right -= 1
+            arr = list(zip(temp.keys(), temp.values()))
+            arr.sort(key=lambda x: (x[1], x[0]))
+            rc = max(rc, len(arr))
 
+            if rc > len(A):
+                for _ in range(rc - len(A)):
+                    A.append([0] * cc)
+            else:
+                for j in range(len(arr), len(A)):
+                    A[j][i] = 0
 
-print(answer[0], answer[1], answer[2])
-            
+            for j in range(len(arr)):
+                A[j][i] = arr[j]
+
 
     
-
-    
-
-
-    
-    
-
-
-
-
         
         
         
