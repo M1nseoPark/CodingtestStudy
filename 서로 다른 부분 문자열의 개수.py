81c173c8f4dbc9a2b0c72be212eb4dc41db03810
M1nseoPark <kjms0820@gmail.com>
2022-12-14 16:37:22 +0900

    [BOJ] 서로 다른 부분 문자열의 개수 / Silver 3 / 10M / 해시

--- 서로 다른 부분 문자열의 개수.py
+++ 서로 다른 부분 문자열의 개수.py
@@ -1,10 +1,18 @@
-from itertools import combinations
-
 s = input()
 n = len(s)
+
+left, right = 0, 0
 answer = set()
 
+# 이중 for문으로 풀 수도 있음 
+while left < n:
+    if right >= n:
+        left += 1
+        right = left
 
-    
+    else:
+        answer.add(s[left:right+1])
+        right += 1
 
+print(len(answer))
     
