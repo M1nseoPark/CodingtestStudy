b0b2e6d5f371f61e842cc3f4478bbd28ac21ebb5
M1nseoPark <kjms0820@gmail.com>
2022-12-28 15:07:58 +0900

    [PGS] 여행경로 / Level 3 / X / DFS

--- 여행경로.py
+++ 여행경로.py
@@ -1,27 +1,22 @@
-from collections import deque
-
 def solution(tickets):
-    visited = [0 for _ in range(len(tickets))]
-    n = (len(tickets) * 2 - 2) // 2 + 2
-    
-    q = deque()
-    q.append(['ICN', 1])
-    tickets.sort()
-    answer = ['ICN']
-    
-    while q:
-        here, v = q.popleft()
-        
-        if v == n:
-            break
-        
-        for i in range(len(tickets)):
-            if (tickets[i][0] == here) and (visited[i] == 0):
-                q.append([tickets[i][1], v+1])
-                answer.append(tickets[i][1])
-                visited[i] = 1
-                break
-    
-    return answer
+    answer = []
+    visited = [False] * len(tickets)
+
+    def dfs(airport, path):
+        if len(path) == len(tickets) + 1:
+            answer.append(path)
+            return
+
+        for idx, ticket in enumerate(tickets):
+            if airport == ticket[0] and visited[idx] = False:
+                visited[idx] = True
+                dfs(ticket[1], path+[ticket[1]])
+                visited[idx] = False
+
+    dfs('ICN', ['ICN'])
+    answer.sort()
+
+    return answer[0]
+
 
 print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
