386d34558999ea18c892c4051c277569c5dbbc67
M1nseoPark <kjms0820@gmail.com>
2023-01-04 10:00:31 +0900

    [BOJ] 가장 긴 바이토닉 부분 수열 / Gold 4 / X / DP

---
+++ 가장 긴 바이토닉 부분 수열.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+dp1 = [1] * n
+for i in range(n):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp1[i] = max(dp1[i], dp1[j]+1)
+
+dp2 = [1] * n
+for i in range(n-1, -1, -1):
+    for j in range(n-1, i, -1):
+        if arr[j] < arr[i]:
+            dp2[i] = max(dp2[i], dp2[j]+1)
+
+
+dp = [0] * n
+for i in range(n):
+    dp[i] = dp1[i] + dp2[i] - 1
+
+
+print(max(dp))
