1cf991e25d1bb84fe710e1db2cad9cb426c27192
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2023-01-08 18:04:08 +0900

    [BOJ] 뱀 / Gold 4 / 50M / 시뮬레이션

--- 뱀_삼성.py
+++ 뱀_삼성.py
@@ -1,64 +1,52 @@
-### 거의 다 맞게 풀었는데 아깝다..
-### 큐는 굳이 2개 만들 필요 없었음
-### BFS라고 꼭 while q 쓰는 거 아님! 잘 생각해보고 코드 짜기
-
 from collections import deque
 
 n = int(input())
 k = int(input())
-#### 인덱스는 0부터 시작하는게 구현하기 편함
-board = [[0] * n for _ in range(n)]  # 0행 0열부터 시작 (그래프를 모두 0으로 채워줌)
-# 사과 위치는 모두 1로 채워줌 -> 이거때매 엄~~청 헤맴
+board = [[0] * n for _ in range(n)]
 for _ in range(k):
     a, b = map(int, input().split())
     board[a-1][b-1] = 1
 
+move = deque()
 l = int(input())
-way = {}  #### 딕셔너리로 구현하면 좋음
 for _ in range(l):
-    a, b = input().split()
-    way[int(a)] = b
+    move.append(list(input().split()))
 
-dx = [1, 0, -1, 0]   # 우, 하, 좌, 상
-dy = [0, 1, 0, -1]
+pos = deque()   # 뱀의 위치
+pos.append([0, 0])
+board[0][0] = 2
+d, t = 1, 1   # 뱀의 방향, 시간
 
-snake = deque()
-snake.append([0, 0])
-board[0][0] = 2  # 뱀이 있는 곳은 2로 채워줌
-time = 1
+dx = [0, 1, 0, -1]
+dy = [-1, 0, 1, 0]
 
-def bfs():
-    global time
-    ny, nx, d = 0, 0, 0
+while True:
+    nx = pos[-1][1] + dx[d]
+    ny = pos[-1][0] + dy[d]
 
-    while True:
-        # 이동하기
-        nx = nx + dx[d]
-        ny = ny + dy[d]
+    # 뱀이 벽과 부딪치면 게임 끝남 
+    if 0 > nx or 0 > ny or nx >= n or ny >= n:
+        break
 
-        if 0 <= nx < n and 0 <= ny < n and board[ny][nx] != 2:
-            if board[ny][nx] == 1:   # 사과를 먹고 길이를 늘림
-                snake.append([ny, nx])
-                board[ny][nx] = 2
+    # 뱀이 자기자신과 부딪치면 게임 끝남 
+    if board[ny][nx] == 2:
+        break
 
-            else:  # 빈칸이면 이동만 하고 꼬리 길이를 줄임
-                board[ny][nx] = 2
-                snake.append([ny, nx])
-                ty, tx = snake.popleft()
-                board[ty][tx] = 0
+    if board[ny][nx] == 0:   # 이동한 칸에 사과가 없다면
+        sy, sx = pos.popleft()
+        board[sy][sx] = 0
 
-            ##### 방향 바꾸기 -> 암기해야 할 코드(5->1 이런 거 생각하느라 시간 꽤 씀)
-            if time in way.keys():
-                if way[time] == 'D':
-                    d = (d + 1) % 4
-                else:
-                    d = (d - 1) % 4
+    pos.append([ny, nx])
+    board[ny][nx] = 2
 
-            time += 1
-
+    # 방향 전환 
+    if len(move) != 0 and int(move[0][0]) == t:
+        x, c = move.popleft()
+        if c == 'L':
+            d = (d + 3) % 4
         else:
-            break
+            d = (d + 1) % 4
 
+    t += 1
 
-bfs()
-print(time)
+print(t)
