3ba2d2b9a619bdf47ed35076410c8f17963084f7
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2023-01-08 15:19:23 +0900

    [BOJ] 가장 긴 증가하는 부분 수열 4 / Gold 4 / X / DP

--- 가장 긴 증가하는 부분 수열 4.py
+++ 가장 긴 증가하는 부분 수열 4.py
@@ -1,27 +1,22 @@
-# 나는 max문을 if문으로 구현하고 해당되는 경우마다 append를 해서 배열을 구했는데
-# 왜 안되는지 모르겠음
-
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [1 for _ in range(n)]
-
+dp = [[] for _ in range(n)]
 for i in range(n):
+    dp[i].append(arr[i])
+
+for i in range(1, n):
     for j in range(i):
         if arr[j] < arr[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
+            if len(dp[j]) + 1 > len(dp[i]):
+                dp[i] = dp[j] + [arr[i]]
 
-print(max(dp))
-x = max(dp)
-
-## 역추적
 answer = []
-for i in range(n-1, -1, -1):
-    if dp[i] == x:
-        answer.append(arr[i])
-        x -= 1
+for i in range(n):
+    if len(dp[i]) > len(answer):
+        answer = dp[i]
 
-answer.reverse()
+answer.sort()
+print(len(answer))
 print(' '.join(map(str, answer)))
-
     
