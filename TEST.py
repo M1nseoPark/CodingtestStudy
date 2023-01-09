eb22f3001aa2adfa0ce0869e93ac2a9ce8b12ea2
M1nseoPark <kjms0820@gmail.com>
2023-01-09 10:49:29 +0900

    backup

--- TEST.py
+++ TEST.py
@@ -1,39 +1,4 @@
-n, k = map(int, input().split())
-arr = list(map(int, input().split()))
 
-left, right = 0, 0
-kt, answer = 0, 1
-
-if arr[left] % 2 == 1:
-    kt += 1
-    answer = 0
-
-while left < n-1 and right < n-1:
-    if kt >= k:
-        if arr[right+1] % 2 == 0:
-            right += 1
-        else:
-            if arr[left] % 2 == 1:
-                kt -= 1
-            left += 1
-
-    else:
-        answer = max(right - left - kt + 1, answer)
-        right += 1
-        if arr[right] % 2 == 1:
-            kt += 1
-
-
-
-
-'''
-1 12 123 
-1 1  2
-
-23 234 2345 
-1  1   2
-'''
-    
-    
-    
         
+        
+    
---
+++ 스티커.py
@@ -0,0 +1,8 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    sticker = []
+    sticker.append(list(map(int, input().split())))
+    sticker.append(list(map(int, input().split())))
+
+    
