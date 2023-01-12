dbde3403eb6b0c1d27e5b56754ddec6ef48b60e0
M1nseoPark <kjms0820@gmail.com>
2023-01-12 21:48:41 +0900

    [BOJ] 타일 채우기 / Gold 4 / X / DP

---
+++ 타일 채우기.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+if n % 2 == 1:
+    print(0)
+else:
+    dp = [0] * (n + 1)
+    dp[2] = 3
+
+    for i in range(4, n+1, 2):
+        dp[i] = dp[i-2] * 3 + 2
+        for j in range(2, i-2, 2):
+            dp[i] += dp[j] * 2
+
+    print(dp[n])
+
+
+# dp[6] = dp[4] * 3 + 2 + dp[2] * 2
+# dp[8] = dp[6] * 3 + 2 + dp[2] * 2 + dp[4] * 2
