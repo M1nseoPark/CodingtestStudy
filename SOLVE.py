acae99e11713f25c8395632199a790d860eedeee
M1nseoPark <kjms0820@gmail.com>
2023-01-13 21:38:03 +0900

    [BOJ] 나무 재테크 / Gold 3 / X / 시뮬레이션

--- SOLVE.py
+++
@@ -1,7 +0,0 @@
-tree = {}
-tree[1] = [1, 1, 1]
-tree[2] = [1, 2, 2]
-
-tree = sorted(tree.items(), key=lambda x: x[1][2])
-print(tree)
-        
--- 나무 재테크.py
+++ 나무 재테크.py
@@ -6,55 +6,45 @@ for _ in range(n):
     A.append(list(map(int, sys.stdin.readline().split())))
 
 nutri = [[5 for _ in range(n)] for _ in range(n)]  # 처음 양분은 모든 칸에 5만큼 있음
-tree = {}
-idx = 1
+tree = [[deque() for _ in range(n)] for _ in range(n)]
 for _ in range(m):
     x, y, z = map(int, sys.stdin.readline().split())
-    tree[idx] = [x-1, y-1, z]
-    idx += 1
+    tree[x-1][y-1].append(z)
 
 dy = [-1, 0, 1, -1, 1, -1, 0, 1]
 dx = [-1, -1, -1, 0, 0, 1, 1, 1]
 
 year = 0
 while year < k:
-    die = []   # 죽은 나무 
-    
-    ## 봄
-    tree.sort(key=lambda x:x[2])  # 나이가 어린 나무부터
-    for i in range(len(tree)):
-        x, y, age = tree[i][0], tree[i][1], tree[i][2]
-        if age != 0:
-            if age <= nutri[x][y]:
-                nutri[x][y] -= age  # 자신의 나이만큼 양분 먹음
-                tree[i][2] += 1  # 나이 1 증가
-            else:   # 먹지 못하면 바로 죽음
-                tree[i][2] = 0  
-                die.append([x, y, age])
-
-
-    ## 여름
-    for i in range(len(die)):
-        x, y, age = die[i][0], die[i][1], die[i][2]
-        nutri[x][y] += (age // 2)
-
-
-    ## 가을
-    for i in range(len(tree)):
-        x, y, age = tree[i][0], tree[i][1], tree[i][2]
-        if age != 0 and age % 5 == 0:  # 나이가 5의 배수인 나무의
-            for j in range(8):
-                nx = x + dx[j]
-                ny = y + dy[j]
-
-                if 0 <= nx < n and 0 <= ny < n:
-                    tree.append([nx, ny, 1])  # 인접한 8개의 칸에 나이가 1인 나무 생김
-
-    ## 겨울
+    ## 봄 + 여름
+    # 입력으로 주어지는 나무 위치는 모두 다름 + 새로 생기는 나무는 리스트 왼쪽에 추가
+    # -> 나이 어린 나무부터 정렬할 필요 없음 
     for i in range(n):
         for j in range(n):
-            nutri[i][j] += A[i][j]   # 땅에 양분 A[r][c] 추가함
+            tlen = len(tree[i][j])
+            for k in range(tlen):
+                if tree[i][j][k] <= nutri[i][j]:
+                    nutri[i][j] -= tree[i][j][k]  # 자신의 나이만큼 양분 먹음
+                    tree[i][j][k] += 1  # 나이 1 증가
+                else:
+                    for _ in range(k, tlen):  # 뒤의 나무는 나이가 큰 나무들이니 무조건 죽음 
+                        nutri[i][j] += tree[i][j].pop() // 2
+                    break
+                
 
+    ## 가을
+    for i in range(n):
+        for j in range(n):
+            for z in tree[i][j]:
+                if z % 5 == 0:
+                    for l in range(8):
+                        nx = i + dx[l]
+                        ny = j + dy[l]
+                        if 0 <= nx < n and 0 <= ny < n:
+                            tree[nx][ny].appendleft(1)
+
+            # 겨울 
+            tree[i][j] += A[i][j]
 
     year += 1
 
