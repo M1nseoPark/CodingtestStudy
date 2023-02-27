f90f0fe85c8f31260d2eb076e61e359a325e4c52
M1nseoPark <kjms0820@gmail.com>
2023-02-27 17:48:41 +0900

    [BOJ] 다리 놓기 / Silver 5 / 10M / 수학

--- TEST.py
+++ TEST.py
@@ -1,38 +1,3 @@
-from itertools import combinations
-import sys
 
-n, m = map(int, input().split())
-city = []
-for _ in range(n):
-    city.append(list(map(int, input().split())))
-
-home = []
-chicken = []
-for i in range(n):
-    for j in range(n):
-        if city[i][j] == 1:
-            home.append([i, j])
-        if city[i][j] == 2:
-            chicken.append([i, j])
-
-
-def count(arr):
-    result = 0
-    for hy, hx in home:
-        cnt = sys.maxsize
-        for cy, cx in arr:
-            if cnt > abs(cy-hy) + abs(cx-hx):
-                cnt = abs(cy-hy) + abs(cx-hx)
-        result += cnt
-
-    return result
-    
-
-answer = sys.maxsize
-for i in range(1, m+1):
-    for c in combinations(chicken, i):
-        answer = min(answer, count(c))
-
-print(answer)
 
 
--- 다리 놓기.py
+++ 다리 놓기.py
@@ -4,14 +4,3 @@ t = int(input())
 for _ in range(t):
     n, m = map(int, input().split())
     print(math.factorial(m) // (math.factorial(n) * math.factorial(m-n)))
-    
-    
-
-
-
-    
-    
-    
-    
-    
-        
