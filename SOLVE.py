d7efc81a3bdf11a76cc940f8a7dd49c0a0bd24ba
M1nseoPark <kjms0820@gmail.com>
2023-02-28 00:01:44 +0900

    [PGS] 인사고과 / Level 3 / X / 정렬

--- SOLVE.py
+++ SOLVE.py
@@ -1,9 +1,10 @@
-import math
 
-t = int(input())
-for _ in range(t):
-    n, m = map(int, input().split())
-    print(math.factorial(m) // (math.factorial(n) * math.factorial(m-n)))
+
+
+        
+        
+        
+        
     
     
 
