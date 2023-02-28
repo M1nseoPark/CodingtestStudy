bdaca7db0bb035ca52378021cf1f56c42913d0ac
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2023-02-28 10:30:19 +0900

    [BOJ] 로봇 청소기 / Gold 5 / 50M / 시뮬레이션

--- 로봇 청소기_삼성.py
+++ 로봇 청소기_삼성.py
@@ -1,50 +1,45 @@
-from collections import deque
-
 n, m = map(int, input().split())
-r, c, d = map(int, input().split())
-maps = []
+ry, rx, d = map(int, input().split())
+room = []
 for _ in range(n):
-    maps.append(list(map(int, input().split())))
-
-q = deque()
-q.append([r, c])
-visited = [[0] * m for _ in range(n)]
-visited[r][c] = 1   # 현재 위치를 청소함
+    room.append(list(map(int, input().split())))
 
 dx = [0, 1, 0, -1]
 dy = [-1, 0, 1, 0]
-answer = 1
 
-def bfs():
-    global d, answer
 
-    while q:
-        y, x = q.popleft()
-        clean = False
-        for i in range(4):
-            d = (d - 1) % 4   # 왼쪽 방향으로 회전
-            nx = x + dx[d]  # 한 칸을 전진
-            ny = y + dy[d]
-            if 0 <= nx < m and 0 <= ny < n and maps[ny][nx] == 0 and visited[ny][nx] == 0:
-                q.append([ny, nx])
-                answer += 1
-                visited[ny][nx] = 1   # 현재 위치를 청소
-                clean = True
-                break
-            else:
-                continue
+def dfs(y, x, d):
+    global answer
 
-        # 네 방향 모두 청소가 이미 되어있거나 벽인 경우,
-        if not clean:
-            nx = x - dx[d]
-            ny = y - dy[d]
-            # 후진을 하고 2번으로 돌아감
-            if 0 <= nx < m and 0 <= ny < n and maps[ny][nx] == 0:
-                q.append([ny, nx])
-            # 후진도 할 수 없는 경우에는 작동을 멈춤
-            else:
-                return
+    if room[y][x] == 0:
+        room[y][x] = 3
+        answer += 1
 
-bfs()
+    idx = 0
+    flag = False
+    ny, nx = 0, 0
+    
+    while idx != 4:
+        d = (d - 1) % 4
+        ny = y + dy[d]
+        nx = x + dx[d]
+
+        if 0 <= ny < n and 0 <= nx < m and room[ny][nx] == 0:
+            flag = True
+            break
+
+        idx += 1
+
+    if flag:
+        dfs(ny, nx, d)
+    else:
+        if room[y-dy[d]][x-dx[d]] != 1:   # 후진 방향 실수해서 한참 헤맴!
+            dfs(y-dy[d], x-dx[d], d)
+        else:
+            return
+
+        
+answer = 0
+dfs(ry, rx, d)
 print(answer)
     
