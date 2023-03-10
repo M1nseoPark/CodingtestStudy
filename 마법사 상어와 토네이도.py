d933470e451034fa36d9f51eb094a935b57a30fc
M1nseoPark <kjms0820@gmail.com>
2023-03-10 15:11:48 +0900

    [BOJ] 마법사 상어와 토네이도 / Gold 3 / 2H / 시뮬레이션

--- 마법사 상어와 토네이도.py
+++ 마법사 상어와 토네이도.py
@@ -3,44 +3,63 @@ board = []
 for _ in range(n):
     board.append(list(map(int, input().split())))
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-size, c, d = 1, 0, 0
+wind = [{(-2, 0):0.02, (-1, -1):0.10, (-1, 0):0.07, (-1, 1):0.01,
+         (0, -2):0.05, (1, -1):0.10, (1, 0):0.07, (1, 1):0.01, (2, 0):0.02}, # 서
+        {(0, -2):0.02, (1, -1):0.10, (0, -1):0.07, (-1, -1):0.01,
+         (2, 0):0.05, (1, 1):0.10, (0, 1):0.07, (-1, 1):0.01, (0, 2):0.02}, # 남 
+        {(2, 0):0.02, (1, 1):0.10, (1, 0):0.07, (1, -1):0.01,
+         (0, 2):0.05, (-1, 1):0.10, (-1, 0):0.07, (-1, -1):0.01, (-2, 0):0.02}, # 동 
+        {(0, 2):0.02, (-1, 1):0.10, (0, 1):0.07, (1, 1):0.01,
+         (-2, 0):0.05, (-1, -1):0.10, (0, -1):0.07, (1, -1):0.01, (0, -2):0.02}] # 북
+rest = [[0, -1], [1, 0], [0, 1], [-1, 0]]
 
-move = {(-2,0):0.02, (-1,-1):0.10, (-1,0):0.07, (-1,1):0.01, (0,-2):0.05,
-        (1,-1):0.10, (1,0):0.07, (1,1):0.01, (2,0):0.02, (0,-1):0.55}
-
-y, x = n//2+1, n//2+1
+y, x, i = n//2, n//2, 0
 answer = 0
 
+dy = [0, 1, 0, -1]
+dx = [-1, 0, 1, 0]
+way = []
+
+a, b = 1, 0
+while True:
+    if len(way) >= n*n:
+        break
+    
+    for _ in range(2):
+        for j in range(a):
+            way.append(b)
+        b += 1
+    a += 1
+
+        
 while True:
     if y == 0 and x == 0:
         break
 
-    for i in range(size):
-        y += dy[d]
-        x += dx[d]
-        
-        sand = board[y][x]
-        board[y][x] = 0
-        
-        for k, v in move.items():
-            ny = y + k[0]
-            nx = x + k[1]
+    d = way[i] % 4
+    y += dy[d]
+    x += dx[d]
+    
+    sand = board[y][x]
+    board[y][x] = 0
+    temp = sand
 
-            if 0 > ny or 0 > nx or ny >= n or nx >= n:
-                answer += int(sand * v)
-            else:
-                board[ny][nx] += int(sand * v)
+    for k, v in wind[d].items():
+        ny = y + k[0]
+        nx = x + k[1]
+        if 0 <= ny < n and 0 <= nx < n:
+            board[ny][nx] += int(sand * v)
+            temp -= int(sand * v)
+        else:
+            answer += int(sand * v)
+            temp -= int(sand * v)
+    
+    if 0 <= y + rest[d][0] < n and 0 <= x + rest[d][1] < n:
+        board[y+rest[d][0]][x+rest[d][1]] += temp
+    else:
+        answer += temp
 
-    c += 1
-    d = (d + 1) % 4
-    if c == 2:
-        c = 0
-        size += 1
+    i += 1
 
-    print(y, x)
-    for i in range(n):
-        print(board[i])
-        
+
 print(answer)
