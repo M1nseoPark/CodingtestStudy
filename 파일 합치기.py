e12130dba00a19afd8e5c37c4ea9d717f2245afc
M1nseoPark <kjms0820@gmail.com>
2023-03-13 09:44:28 +0900

    [BOJ] 파일 합치기 / Gold 3 / X / DP

--- 파일 합치기.py
+++ 파일 합치기.py
@@ -1,7 +1,9 @@
-test = int(input())
+import sys
+
+test = int(sys.stdin.readline())
 for _ in range(test):
-    k = int(input())   # 소설을 구성하는 장의 개수 
-    file = list(map(int, input().split()))   # 1장부터 K장까지 수록된 파일 크기
+    k = int(sys.stdin.readline())   # 소설을 구성하는 장의 개수 
+    file = list(map(int, sys.stdin.readline().split()))   # 1장부터 K장까지 수록된 파일 크기
 
     # dp[a][b] = a부터 b까지의 그룹의 압축 최소 비용
     dp = [[0] * k for _ in range(k)]
@@ -10,11 +12,8 @@ for _ in range(test):
         dic[i] = dic[i-1] + file[i]
 
     for size in range(1, k):   # 그룹의 크기
-        for start in range(k-1):   # 그룹의 시작 인덱스 범위는 0부터 k-2까지
+        for start in range(k-size):   # 그룹의 시작 인덱스 범위는 0부터 k-2까지
             end = start + size
-
-            if end >= len(files):
-                break
 
             rst = float('inf')
             for i in range(start, end):
