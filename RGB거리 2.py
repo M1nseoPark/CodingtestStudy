043582268d346f7a66fc83e0f133479e9972c1c6
M1nseoPark <kjms0820@gmail.com>
2023-03-15 15:39:11 +0900

    [BOJ] RGB거리 2 / Gold 4 / X / DP

--- RGB거리 2.py
+++ RGB거리 2.py
@@ -3,9 +3,20 @@ color = []
 for _ in range(n):
     color.append(list(map(int, input().split())))
 
-for i in range(1, n):
-    color[i][0] += min(color[i-1][1], color[i-1][2])
-    color[i][1] += min(color[i-1][0], color[i-1][2])
-    color[i][2] += min(color[i-1][0], color[i-1][1])
+answer = float('inf')
+for i in range(3):
+    dp = [[-1] * 3 for _ in range(n+1)]
+    dp[0] = [float('inf'), float('inf'), float('inf')]
+    dp[0][i] = color[0][i]
 
-print(min(color[n-1]))
+    for i in range(1, n):
+        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + color[i][0]
+        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + color[i][1]
+        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + color[i][2]
+
+    dp[n-1][i] = float('inf')
+    answer = min(answer, min(dp[n-1]))
+
+print(answer)
+
+
