33902842a6465c34f701a83060406995fae8c629
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2023-03-20 08:59:56 +0900

    [BOJ] 새로운 게임 2 / Gold 2 / 1H / 시뮬레이션

--- 새로운 게임 2.py
+++ 새로운 게임 2.py
@@ -1,63 +1,60 @@
 n, k = map(int, input().split())
-chess = []
+board = []
 for _ in range(n):
-    chess.append(list(map(int, input().split())))
-
-board = [[[] for _ in range(n)] for _ in range(n)]
-mark = {}
+    board.append(list(map(int, input().split())))
+mark = [[[] for _ in range(n)] for _ in range(n)]
+dic = {}
 for i in range(1, k+1):
-    y, x, d = map(int, input().split())
-    mark[i] = [y-1, x-1, d-1]
-    board[y-1][x-1].append(i)
+    a, b, c = map(int, input().split())
+    dic[i] = [a-1, b-1, c-1]
+    mark[a-1][b-1].append(i)
 
-dx = [1, -1, 0, 0]   # 동, 서, 북, 남 
+dx = [1, -1, 0, 0]   # 동서북남 
 dy = [0, 0, -1, 1]
-
 answer = 1
+
 while True:
     if answer > 1000:
+        answer = -1
         break
 
     end = False
+    
     for i in range(1, k+1):
-        y, x, d = mark[i]
-        ny = y + dy[d]
-        nx = x + dx[d]
-
-        # 체스판 벗어나는 경우 or 파란색 
-        if ny >=n or nx >= n or nx < 0 or ny < 0 or chess[ny][nx] == 2:
+        y, x, d = dic[i]
+        if 0 > y+dy[d] or 0 > x+dx[d] or x+dx[d] >= n or y+dy[d] >= n or board[y+dy[d]][x+dx[d]] == 2:
             if d == 0 or d == 2:
                 d += 1
             else:
                 d -= 1
 
-            ny = y + dy[d]
-            nx = x + dx[d]
-            if ny >= n or nx >= n or nx < 0 or ny < 0 or chess[ny][nx] == 2:
-                nx = x
-                ny = y
-
-        mark[i] = [ny, nx, d]
-        if ny == y and nx == x:
+        ny = y + dy[d]
+        nx = x + dx[d]
+        if 0 > ny or 0 > nx or ny >= n or nx >= n or board[ny][nx] == 2:
+            dic[i] = [y, x, d]
             continue
 
-        idx = board[y][x].index(i)
-        # 해당 말 위에 있던 말 이동
-        for j in board[y][x][idx+1:]:   
-            mark[j][0] = ny
-            mark[j][1] = nx
+        elif board[ny][nx] == 0:
+            dic[i][2] = d
+            idx = mark[y][x].index(i)
+            for j in range(idx, len(mark[y][x])):
+                nd = dic[mark[y][x][j]][2]
+                mark[ny][nx].append(mark[y][x][j])
+                dic[mark[y][x][j]] = [ny, nx, nd]
 
-        # 옮긴 뒤의 색이 하얀색
-        if chess[ny][nx] == 0:
-            board[ny][nx] += board[y][x][idx:]
+            mark[y][x] = mark[y][x][:idx]
 
-        # 옮긴 뒤의 색이 빨간색
-        elif chess[ny][nx] == 1:
-            board[ny][nx] += board[y][x][idx:][::-1]
+        else:
+            dic[i][2] = d
+            idx = mark[y][x].index(i)
+            for j in range(len(mark[y][x])-1, idx-1, -1):
+                nd = dic[mark[y][x][j]][2]
+                mark[ny][nx].append(mark[y][x][j])
+                dic[mark[y][x][j]] = [ny, nx, nd]
 
-        board[y][x] = board[y][x][:idx]
+            mark[y][x] = mark[y][x][:idx]
 
-        if 0 <= ny < n and 0 <= nx < n and len(board[ny][nx]) >= 4:
+        if len(mark[ny][nx]) >= 4:
             end = True
             break
 
@@ -66,12 +63,5 @@ while True:
 
     answer += 1
 
-
-if answer > 1000:
-    print(-1)
-else:
-    print(answer)
-        
-            
-        
+print(answer)
 
