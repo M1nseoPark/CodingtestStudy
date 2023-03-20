c2245a6bd4ce5b85c0e2fd2cd2f17ea676aad7c3
M1nseoPark <kjms0820@gmail.com>
2023-03-20 10:53:02 +0900

    [BOJ] 사다리 조작 / Gold 3 / X / 시뮬레이션

--- 사다리 조작.py
+++ 사다리 조작.py
@@ -1,52 +1,50 @@
-# 가로선 연속하면 안됨
-# 세로선, 가로선 인덱스 1부터 시작 
-# i번 세로선의 결과가 i번이 나오기 위해 추가해야 하는 가로선 개수의 최솟값
-
-n, m, h = map(int, input().split())  # 세로선 개수, 이미 그려진 가로선 개수, 가로선 놓을 수 있는 위치의 개수
-graph = [[0] * n for _ in range(h)]
-
+n, m, h = map(int, input().split())
+line = [[0] * (n + 1) for _ in range(h+1)]
 for _ in range(m):
-    a, b = map(int, input().split())   # 가로선, 세로선 
-    graph[a-1][b-1] = 1
+    a, b = map(int, input().split())
+    line[a][b] = 1
+
+sub = []   # 사다리 놓을 수 있는 위치 후보 
+for i in range(1, h+1):
+    for j in range(1, n):
+        if line[i][j] == 0 and line[i][j-1] == 0 and line[i][j+1] == 0:
+            sub.append([i, j])
+
 
 def check():
-    for i in range(n):   # 세로선 
-        temp = i   # 현재 위치 
-        for j in range(h):   # 가로선 
-            if graph[j][temp] == 1:   # 1이면 오른쪽 세로선으로 이동
-                temp += 1
-            elif temp > 0 and graph[j][temp-1] == 1:   # 왼쪽이 1인 경우
-                temp -= 1
+    for i in range(1, n+1):
+        now = i   # 현재 몇번 세로선에 있는지 
+        for j in range(1, h+1):   # 사다리 맨위에서 맨아래까지 
+            if line[j][now] == 1:
+                now += 1
+            elif line[j][now-1] == 1:
+                now -= 1
 
-        if temp != i:
+        if i != now:   # i번 세로선의 결과가 i번이 아니면 
             return False
-
     return True
-                
-                
-def dfs(cnt, x, y):
+    
+
+def dfs(cnt, num):   # 놓아야 하는 사다리 수, 몇번째 후보까지 검사했는지 
     global answer
-    if check():   # i번 세로선의 결과가 i번이 나오면 
-        answer = min(answer, cnt)
-        return
-    if cnt == 3 or answer <= cnt:   # cnt가 3이면 다음 호출에서 4가 돼버림, 가로선을 정답보다 많이 만든 경우 확인 필요 X
+    if cnt >= answer:   # 사다리를 예전에 구한 정답보다 많이 놓아야 하면
         return
 
-    for i in range(x, h):  # x~h까지 가로선 
-        k = y if i == x else 0   # 가로선이 변경되기 전까진 가로선을 계속해서 탐색, 행이 변경되면 처음부터 탐색
-        for j in range(k, n-1):
-            if graph[i][j] == 0 and graph[i][j+1] == 0:   # 가로선 연속하면 안되므로
-                if j > 0 and graph[i][j-1] == 1:   # 가로선 연속하면 안되므로 
-                    continue
-                
-                graph[i][j] = 1   # 가로선 놓기
-                dfs(cnt+1, i, j+2)
-                graph[i][j] = 0
-                
-        
-answer = 4   # 정답은 3 이하
-dfs(0, 0, 0)
-if answer <= 3:
+    if check():
+        answer = cnt
+        return
+
+    for idx in range(num+1, len(sub)):
+        i, j = sub[idx]
+        if line[i][j-1] == 0 and line[i][j+1] == 0:
+            line[i][j] = 1
+            dfs(cnt+1, idx)
+            line[i][j] = 0
+
+            
+answer = 4
+dfs(0, -1)
+if answer < 4:
     print(answer)
 else:
     print(-1)
