035c6e098d07b3c347a0e1aceb81e9802fc5a257
M1nseoPark <kjms0820@gmail.com>
2023-04-11 10:15:12 +0900

    [BOJ] 연속부분최대곱 / Silver 4 / 13M / DP

--- SOLVE.py
+++ SOLVE.py
@@ -1,3 +1,51 @@
+import heapq
 
+n = int(input())
+m = int(input())
+bus = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    bus[a].append([b, c])  # 도착점, 비용 
+start, end = map(int, input().split())
+
+
+def dijkstra(start):
+    dist = [float('inf')] * (n + 1)
+    prev = [0] * (n + 1)
+    dist[start] = 0
+    minHeap = []
+    heapq.heappush(minHeap, [0, start])   # 비용, 위치 
+
+    while minHeap:
+        d, v = heapq.heappop(minHeap)
+
+        if dist[v] != d:
+            continue
+
+        for i in bus[v]:
+            cost = d + i[1]
+            if cost < dist[i[0]]:
+                dist[i[0]] = cost
+                prev[i[0]] = v
+                heapq.heappush(minHeap, [cost, i[0]])
+
+    return dist, prev
+
+dist, prev = dijkstra(start)
+print(dist[end])
+result = [end]
+cur = end
+
+while cur != start:
+    result.append(prev[cur])
+    cur = prev[cur]
+
+result = result[::-1]
+print(len(result))
+print(' '.join(map(str, result)))
+
+
+
+        
 
     
--- 나무박멸.py
+++ 나무박멸.py
@@ -1,115 +1,105 @@
-# nxn, 나무의 그루 수+벽 정보
-# 3) 제초제를 뿌렸을 때 나무가 가장 많이 박멸되는 칸에 뿌림 (행, 열 -> 람다 정렬)
-# 3-1) 제초제는 4개의 대각선 방향으로 k칸만큼 전파됨
-# 3-2) 벽이나 빈칸 만나면 이 칸까지만 제초제 뿌려지고 이후로는 전파 안됨
-# 3-3) 제초제가 뿌려진 칸은 c년만큼 제초제가 남아있다가 c+1년째에 사라짐 
-from collections import deque
-
 n, m, k, c = map(int, input().split())
 board = []
 for _ in range(n):
-    board.append(list(map(int, input().split())))   # 빈칸 0, 벽 -1, 제초제 -2
+    board.append(list(map(int, input().split())))   # 나무 1~~100, 빈칸 0, 벽 -1
 
-tree = {}
-for i in range(n):
-    for j in range(n):
-        if board[i][j] > 0:
-            tree[(i, j)] = board[i][j]
+dx = [-1, 0, 1, 0, -1, 1, -1, 1]
+dy = [0, -1, 0, 1, -1, 1, 1, -1]
+answer = 0
 
-dx = [0, 0, -1, 1, -1, 1, -1, 1]
-dy = [-1, 1, 0, 0, 1, -1, -1, 1]
-
-# 제초제를 어느 칸에 뿌릴지 결정 
+# 제초제 뿌릴 칸 선택 
 def pick():
-    result = []
+    killer = []
+    for y in range(n):
+        for x in range(n):
+            if board[y][x] > 0:
+                kill = board[y][x]
+                for i in range(4, 8):
+                    for j in range(1, k+1):
+                        ny = y + dy[i] * j
+                        nx = x + dx[i] * j
 
-    for key, val in tree.items():
-        y, x = key
-        result.append([val, y, x])
+                        if 0 <= ny < n and 0 <= nx < n and board[ny][nx] > 0:
+                            kill += board[ny][nx]
+                        else:
+                            break
+                
+                killer.append([kill, y, x])
+            else:
+                killer.append([0, y, x])
+    
+    killer.sort(key=lambda x:(-x[0], x[1], x[2]))
+    return killer[0]
+
+
+for _ in range(m):
+    grow = []
+    breed = []
+    num = []
+    killed = []
+
+    # 나무 성장, 번식 
+    for y in range(n):
+        for x in range(n):
+            if board[y][x] > 0:
+                cnt1, cnt2 = 0, 0
+                breed.append([])
+                for i in range(4):
+                    ny = y + dy[i]
+                    nx = x + dx[i]
+                    if 0 <= ny < n and 0 <= nx < n:
+                        if board[ny][nx] > 0:
+                            cnt1 += 1
+                        if board[ny][nx] == 0:
+                            breed[-1].append([ny, nx])
+                            cnt2 += 1
+
+                if cnt2 > 0:
+                    num.append((board[y][x] + cnt1) // cnt2)
+                else:
+                    num.append(0)
+                    
+                if cnt1 > 0:
+                    grow.append([y, x, cnt1])
+
+    for g in grow:
+        y, x, cnt = g[0], g[1], g[2]
+        board[y][x] += cnt
+    
+    for i in range(len(breed)):
+        if len(breed[i]) > 0:
+            for b in breed[i]:
+                y, x = b[0], b[1]
+                board[y][x] += num[i]
+    
+    kd, ky, kx = pick()
+    if kd != 0:
+        killed.append([[ky, kx]])
+        answer += board[ky][kx]
+        board[ky][kx] = -2
 
         for i in range(4, 8):
             for j in range(1, k+1):
-                nx = x + dx[i] * j
-                ny = y + dy[i] * j
-                if 0 > ny or 0 > nx or ny >= n or nx >= n:
-                    break
-                
-                if board[ny][nx] == 0 or board[ny][nx] == -1:
-                    break
-                
-                result[-1][0] += board[ny][nx]
-    
-    result.sort(key=lambda x:(-x[0], x[1], x[2]))
-    return result[0]
+                ny = ky + dy[i] * j
+                nx = kx + dx[i] * j
 
-
-answer = 0
-killer = deque()
-for year in range(m):
-    # 인접한 4개의 칸 중 나무가 있는 칸의 수만큼 나무가 성장함
-    for key, val in tree.items():
-        y, x = key
-        cnt = 0
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= ny < n and 0 <= nx < n and board[ny][nx] > 0:
-                cnt += 1
-        tree[key] += cnt
-        board[y][x] += cnt
+                if 0 <= ny < n and 0 <= nx < n:
+                    if board[ny][nx] > 0:
+                        answer += board[ny][nx]
+                        board[ny][nx] = -2
+                        killed[-1].append([ny, nx])
+                    else:
+                        board[ny][nx] = -2
+                        killed[-1].append([ny, nx])
+                        break
+    else:
+        killed.append([[ky, kx]])
+        board[ky][kx] = -2
     
-    # 인접한 4개의 칸 중 빈칸에 번식 (나무 그루 수 // 번식 가능한 칸의 개수)
-    breed = []
-    for key, val in tree.items():
-        y, x = key
-        temp = []
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= ny < n and 0 <= nx < n and board[ny][nx] == 0:
-                temp.append([ny, nx])
-
-        if len(temp) > 0:
-            cnt = val // len(temp)
-            for by, bx in temp:
-                breed.append([by, bx, cnt])
-    
-    for by, bx, bz in breed:
-        if (by, bx) in tree:
-            tree[(by, bx)] += bz
-        else:
-            tree[(by, bx)] = bz
-        board[by][bx] += bz
-    
-    _, ky, kx = pick()
-
-    # 제초제 뿌림 
-    kill = [[ky, kx]]
-    answer += board[ky][kx]
-    board[ky][kx] = -2
-    del tree[(ky, kx)]
-
-    for i in range(4, 8):
-        for j in range(1, k+1):
-            nx = kx + dx[i] * j
-            ny = ky + dy[i] * j
-            if 0 > ny or 0 > nx or ny >= n or nx >= n:
-                break
-                
-            if board[ny][nx] == 0 or board[ny][nx] == -1:
-                kill.append([ny, nx])
-                board[ny][nx] = -2
-                break
-                
-            kill.append([ny, nx])
-            answer += board[ny][nx]
-            del tree[(ny, nx)]
-            board[ny][nx] = -2
-    
-    killer.append(kill)
-    if year > c:
-        kill = killer.popleft()
-        for ky, kx in kill:
-            board[ky][kx] = 0
+    if len(killed) > c:
+        clean = killed.pop(0)
+        for y, x in clean:
+            board[y][x] = 0
 
 print(answer)
+
