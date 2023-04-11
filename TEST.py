058f09a380fcf9dba310db49db20e98c263e64ef
M1nseoPark <kjms0820@gmail.com>
2023-04-11 11:29:33 +0900

    [PGS] 가장 큰 수 / Level 2 / X / 정렬

--- TEST.py
+++ TEST.py
@@ -1,1 +1,12 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(float(input()))
 
+dp = [0] * n
+dp[0] = arr[0]
+
+for i in range(1, n):
+    dp[i] = max(arr[i], dp[i-1] * arr[i])
+
+print("{:.3f}".format(1.1700000000000002))
