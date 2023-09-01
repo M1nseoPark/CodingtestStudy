6fa49effc6d35d74b40107bd8c7a8999c771de4a
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2023-09-01 10:10:16 +0900

    Update 프로세스.py

--- 프로세스.py
+++ 프로세스.py
@@ -1,27 +1,53 @@
 from collections import deque
 
-def solution(priorities, location):
-    answer = 1
-    q = deque(priorities)
+# 첫 번째 풀이 
+# def solution(priorities, location):
+#     answer = 1
+#     q = deque(priorities)
     
-    while len(q) > 1:
-        cur = q.popleft()
+#     while len(q) > 1:
+#         cur = q.popleft()
         
-        if cur < max(q):
-            q.append(cur)
-            if location == 0:
-                location = len(q) - 1
-            else:
-                location -= 1
-        else:
-            if location == 0:
-                return answer
-            else:
-                answer += 1
-                location -= 1
+#         if cur < max(q):
+#             q.append(cur)
+#             if location == 0:
+#                 location = len(q) - 1
+#             else:
+#                 location -= 1
+#         else:
+#             if location == 0:
+#                 return answer
+#             else:
+#                 answer += 1
+#                 location -= 1
         
-        #print(q, location, answer)
+#         #print(q, location, answer)
             
-    return answer
+#     return answer
 
 #solution([1, 1, 1, 2], 2)
+
+
+# 두 번째 풀이 
+def solution(priorities, location):
+	stack = deque()
+	for i in range(len(priorities)):
+		stack.append([priorities[i], i])
+	
+	answer = 0
+	while True:
+		target = stack.popleft()
+		
+		flag = True
+		for i in range(len(stack)):
+			if target[0] < stack[i][0]:
+				flag = False
+				break
+		if flag:
+			answer += 1
+			if target[1] == location:
+				break
+		else:
+			stack.append(target)
+            
+	return answer
