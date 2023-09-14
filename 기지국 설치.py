d015149d696530fb9edc014d19a562d4350b1b6a
M1nseoPark <kjms0820@gmail.com>
2023-09-14 09:39:57 +0900

    [PGS] 기지국 설치 / Level 3 / X / 수학

--- 기지국 설치.py
+++ 기지국 설치.py
@@ -2,29 +2,17 @@ import math
 
 def solution(n, stations, w):
     answer = 0
-    signal = []
-    v = w * 2 + 1
+    m = w * 2 + 1
     
+    start = 1
     for i in range(len(stations)):
-        s, e = 1, n
-        if stations[i] - w > 0:
-            s = stations[i] - w
-        
-        if stations[i] + w <= n:
-            e = stations[i] + w
-        
-        signal.append([s, e])
+        temp = stations[i] - w - start
+        if temp > 0:
+            answer += math.ceil(temp / m)
+        start = stations[i] + w + 1
     
-    idx = 1
-    
-    for i in range(len(signal)):
-        if idx > n:
-            break
-        temp = signal[i][0] - idx
-        answer += math.ceil(temp / v)
-        idx = signal[i][1] + 1
-    
-    if idx <= n:
-        answer += math.ceil((n - signal[-1][1]) / v)
-        
+    if start <= n:
+        temp = n - start + 1
+        answer += math.ceil(temp / m)
+
     return answer
