1caa1bb18a125fbef5cf7097b380bfb315945d1f
M1nseoPark <kjms0820@gmail.com>
2023-09-24 14:39:08 +0900

    [BFS] 여행 가자 / Gold 4 / X / 유니온 파인드

--- 여행 가자.py
+++ 여행 가자.py
@@ -1,3 +1,4 @@
+## BFS
 from collections import deque
 
 n = int(input())
@@ -42,3 +43,39 @@ else:
     print('NO')
 
 
+## 유니온 파인드 
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n = int(input())
+m = int(input())
+board = []
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+
+plan = list(map(int, input().split()))
+parent = [i for i in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:
+            union_parent(parent, i, j)
+
+answer = 'YES'
+for i in range(1, m):
+    if parent[plan[0]-1] != parent[plan[i]-1]:
+        answer = 'NO'
+        break
+
+print(answer)
