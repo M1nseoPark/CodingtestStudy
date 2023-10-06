c4af9becc924387f91cdd230cd4a6e65b7872ee4
M1nseoPark <kjms0820@gmail.com>
2023-10-06 15:07:37 +0900

    [BOJ] 소트인사이드 / Silver 5 / 10M / 정렬

---
+++ Main/소트인사이드.java
@@ -0,0 +1,16 @@
+// import java.util.*;
+
+// public class Main {
+// public static void main(String[] args) {
+// Scanner in = new Scanner(System.in);
+// int n = in.nextInt();
+
+// String st = String.valueOf(n);
+// String[] arr = st.split("");
+
+// Arrays.sort(arr, Collections.reverseOrder());
+// for (int i = 0; i < st.length(); i++) {
+// System.out.print(Integer.parseInt(arr[i]));
+// }
+// }
+// }
