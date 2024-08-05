ace2f73378e565537ca73b01ee2a5ad9ee3b5476
minseo <kjms0820@gmail.com>
2024-08-05 22:00:15 +0900

    [BOJ] 사칙연산 / Bronze 5 / 1M / 수학

--- Main/Main.java
+++ Main/Main.java
@@ -1,6 +1,17 @@
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] arg) {
-        System.out.print("Hello World!");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
     }
 }
