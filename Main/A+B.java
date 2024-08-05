12c66f7ec06b45e31d0c9f189eb4b924a4e952ef
minseo <kjms0820@gmail.com>
2024-08-05 21:25:49 +0900

    [BOJ] A+B / Bronze 5 / 2M / 수학

---
+++ Main/A+B.java
@@ -0,0 +1,11 @@
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] arg) {
+//         Scanner in = new Scanner(System.in);
+//         int a = in.nextInt();
+//         int b = in.nextInt();
+
+//         System.out.print(a + b);
+//     }
+// }
--- Main/Main.java
+++ Main/Main.java
@@ -3,19 +3,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] arg) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int x = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
 
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            int num = in.nextInt();
-            if (num < x)
-                arr.add(num);
-        }
-
-        for (int i = 0; i < arr.size(); i++) {
-            System.out.print(Integer.toString(arr.get(i)) + " ");
-        }
+        System.out.print(a + b);
     }
 }
