30e98da52929d8e8f36a50ec6ecc2128b49f662b
minseo <kjms0820@gmail.com>
2024-08-05 21:29:04 +0900

    [BOJ] Hello World / Bronze 5 / 1M / 구현

--- Main/Main.java
+++ Main/Main.java
@@ -1,11 +1,6 @@
-import java.util.*;
 
 public class Main {
     public static void main(String[] arg) {
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-
-        System.out.print(a + b);
+        System.out.print("Hello World!");
     }
 }
