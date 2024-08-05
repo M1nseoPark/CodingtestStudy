31366ba26fcab8d14408bf934b39b171eba28f88
minseo <kjms0820@gmail.com>
2024-08-05 21:21:15 +0900

    [BOJ] X보다 작은 수 / Bronze 5 / 15M / 구현

---
+++ Main/X보다 작은 수.java
@@ -0,0 +1,21 @@
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] arg) {
+//         Scanner in = new Scanner(System.in);
+//         int n = in.nextInt();
+//         int x = in.nextInt();
+
+//         ArrayList<Integer> arr = new ArrayList<>();
+
+//         for (int i = 0; i < n; i++) {
+//             int num = in.nextInt();
+//             if (num < x)
+//                 arr.add(num);
+//         }
+
+//         for (int i = 0; i < arr.size(); i++) {
+//             System.out.print(Integer.toString(arr.get(i)) + " ");
+//         }
+//     }
+// }
