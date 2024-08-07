d25c70c75ce17da2837181d75a6ee7148e7ed5b8
minseo <kjms0820@gmail.com>
2024-08-07 22:01:50 +0900

    [BOJ] 윤년 / Bronze 5 / 2M / 수학

--- Main/Main.java
+++ Main/Main.java
@@ -5,14 +5,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] arg) {
         Scanner in = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<>();
+        int year = in.nextInt();
 
-        for (int i = 0; i < 3; i++) 
-            arr.add(in.nextInt());
-
-        arr.sort(Comparator.naturalOrder());
-
-        for (int i = 0; i < 3; i++)
-            System.out.print(arr.get(i) + " ");
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            System.out.println("1");
+        else
+            System.out.println("0");
     }
 }
