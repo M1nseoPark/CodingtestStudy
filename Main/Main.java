2d7e8cac980914047a8d791ffa2e254b806d48b8
minseo <kjms0820@gmail.com>
2024-08-13 22:11:14 +0900

    [BOJ] 대표값2 / Bronze 2 / 5M / 수학

--- Main/Main.java
+++ Main/Main.java
@@ -5,24 +5,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] arg) {
         Scanner in = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
         int sum = 0;
-        int min = 100;
 
-        for (int i = 0; i < 7; i++) {
+        for (int i = 0; i < 5; i++) {
             int x = in.nextInt();
-
-            if (x % 2 == 1) {
-                if (x < min)
-                    min = x;
-                sum += x;
-            }
+            list.add(x);
+            sum += x;
         }
 
-        if (sum == 0)
-            System.out.println("-1");
-        else {
-            System.out.println(sum);
-            System.out.println(min);
-        }
+        list.sort(Comparator.naturalOrder());
+
+        System.out.println(sum / 5);
+        System.out.println(list.get(2));
     }
 }
---
+++ Main/대표값2.java
@@ -0,0 +1,21 @@
+// import java.util.*;
+
+
+// public class Main {
+//     public static void main(String[] arg) {
+//         Scanner in = new Scanner(System.in);
+//         List<Integer> list = new ArrayList<>();
+//         int sum = 0;
+
+//         for (int i = 0; i < 5; i++) {
+//             int x = in.nextInt();
+//             list.add(x);
+//             sum += x;
+//         }
+
+//         list.sort(Comparator.naturalOrder());
+
+//         System.out.println(sum / 5);
+//         System.out.println(list.get(2));
+//     }
+// }
