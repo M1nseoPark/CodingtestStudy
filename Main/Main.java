a099f61e03aad7ca64aa4610a4b470012633891d
minseo <kjms0820@gmail.com>
2024-08-13 21:54:28 +0900

    [BOJ] 홀수 / Bronze 3 / 5M / 수학

--- Main/Main.java
+++ Main/Main.java
@@ -5,27 +5,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] arg) {
         Scanner in = new Scanner(System.in);
+        int sum = 0;
+        int min = 100;
 
-        for (int i = 0; i < 3; i++) {
-            String input = in.nextLine();
-            String[] arr = input.split(" ");
-            int cnt = 0;
+        for (int i = 0; i < 7; i++) {
+            int x = in.nextInt();
 
-            for (int j = 0; j < 4; j++) {
-                if (arr[j].equals("0"))
-                    cnt++;
+            if (x % 2 == 1) {
+                if (x < min)
+                    min = x;
+                sum += x;
             }
+        }
 
-            if (cnt == 4)
-                System.out.println("D");
-            else if (cnt == 3)
-                System.out.println("C");
-            else if (cnt == 2)
-                System.out.println("B");
-            else if (cnt == 1)
-                System.out.println("A");
-            else
-                System.out.println("E");
+        if (sum == 0)
+            System.out.println("-1");
+        else {
+            System.out.println(sum);
+            System.out.println(min);
         }
     }
 }
---
+++ Main/홀수.java
@@ -0,0 +1,27 @@
+// import java.util.*;
+
+
+// public class Main {
+//     public static void main(String[] arg) {
+//         Scanner in = new Scanner(System.in);
+//         int sum = 0;
+//         int min = 100;
+
+//         for (int i = 0; i < 7; i++) {
+//             int x = in.nextInt();
+
+//             if (x % 2 == 1) {
+//                 if (x < min)
+//                     min = x;
+//                 sum += x;
+//             }
+//         }
+
+//         if (sum == 0)
+//             System.out.println("-1");
+//         else {
+//             System.out.println(sum);
+//             System.out.println(min);
+//         }
+//     }
+// }
