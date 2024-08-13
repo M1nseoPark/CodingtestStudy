64bc476ea89ff64c5ad3f39bdd65f8a8a55fc455
minseo <kjms0820@gmail.com>
2024-08-13 21:22:03 +0900

    [BOJ] 주사위 세개 / Bronze 4 / 5M / 수학

--- Main/Main.java
+++ Main/Main.java
@@ -5,11 +5,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] arg) {
         Scanner in = new Scanner(System.in);
-        int year = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
 
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-            System.out.println("1");
+        if (a == b && b == c && a == c)
+            System.out.println(10000 + a * 1000);
+        else if (a == b && b != c)
+            System.out.println(1000 + a * 1000);
+        else if (b == c && a != b)
+            System.out.println(1000 + b * 1000);
+        else if (a == c && b != c)
+            System.out.println(1000 + a * 1000);
         else
-            System.out.println("0");
+            System.out.println(Math.max(a, Math.max(b, c)) * 100);
     }
 }
---
+++ Main/주사위 세개.java
@@ -0,0 +1,22 @@
+// import java.util.*;
+
+
+// public class Main {
+//     public static void main(String[] arg) {
+//         Scanner in = new Scanner(System.in);
+//         int a = in.nextInt();
+//         int b = in.nextInt();
+//         int c = in.nextInt();
+
+//         if (a == b && b == c && a == c)
+//             System.out.println(10000 + a * 1000);
+//         else if (a == b && b != c)
+//             System.out.println(1000 + a * 1000);
+//         else if (b == c && a != b)
+//             System.out.println(1000 + b * 1000);
+//         else if (a == c && b != c)
+//             System.out.println(1000 + a * 1000);
+//         else
+//             System.out.println(Math.max(a, Math.max(b, c)) * 100);
+//     }
+// }
