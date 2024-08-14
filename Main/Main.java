eb658502583cc6530f45d68c1bc17934f6113981
minseo <kjms0820@gmail.com>
2024-08-14 23:07:09 +0900

    [BOJ] 별 찍기 - 2 / Bronze 4 / 3M / 구현

--- Main/Main.java
+++ Main/Main.java
@@ -9,6 +9,6 @@ public class Main {
         int n = in.nextInt();
 
         for (int i = 1; i < n+1; i++) 
-            System.out.println("*".repeat(i));
+            System.out.println(" ".repeat(n-i) + "*".repeat(i));
     }
 }
---
+++ Main/별 찍기 - 2.java
@@ -0,0 +1,13 @@
+// import java.util.*;
+
+
+// public class Main {
+//     public static void main(String[] arg) {
+//         Scanner in = new Scanner(System.in);
+
+//         int n = in.nextInt();
+
+//         for (int i = 1; i < n+1; i++) 
+//             System.out.println(" ".repeat(n-i) + "*".repeat(i));
+//     }
+// }
