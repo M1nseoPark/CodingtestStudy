11c75c4fc687d4d629cb61c8ef784e5d8ee3892d
minseo <kjms0820@gmail.com>
2024-08-14 23:00:47 +0900

    [BOJ] 별 찍기 - 1 / Bronze 5 / 3M / 구현

--- Main/Main.java
+++ Main/Main.java
@@ -1,27 +1,14 @@
 
-import java.io.*;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] arg) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Scanner in = new Scanner(System.in);
 
-        try {
-            int test = Integer.parseInt(br.readLine());
+        int n = in.nextInt();
 
-            for (int i = 0; i < test; i++) {
-                String[] input = br.readLine().split(" ");
-                int a = Integer.parseInt(input[0]);
-                int b = Integer.parseInt(input[1]);
-
-                bw.write((a + b) + "");
-                bw.write("\n");
-            }
-        
-            bw.flush();
-            bw.close();
-        } 
-        catch (Exception e) { }
+        for (int i = 1; i < n+1; i++) 
+            System.out.println("*".repeat(i));
     }
 }
