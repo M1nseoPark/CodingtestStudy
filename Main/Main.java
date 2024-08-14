cddb16e31e92ec515979fadba9fdd5f24964b3f2
minseo <kjms0820@gmail.com>
2024-08-14 22:32:22 +0900

    [BOJ] 카드 역배치 / Bronze 2 / 5M / 구현

--- Main/Main.java
+++ Main/Main.java
@@ -5,23 +5,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] arg) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int y = 0;
-        int m = 0;
+        List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            int time = in.nextInt();
+        for (int i = 1; i < 21; i++)
+            list.add(i);
 
-            y += (time / 30 + 1) * 10;
-            m += (time / 60 + 1) * 15;
+        for (int i = 0; i < 10; i++) {
+            int s = in.nextInt();
+            int e = in.nextInt();
+
+            Collections.reverse(list.subList(s-1, e));
         }
         
-        if (y == m) 
-            System.out.println("Y M " + y);
-        else if (y < m)
-            System.out.println("Y " + y);
-        else
-            System.out.println("M " + m);
-    
+        for (int i = 0; i < 20; i++) 
+            System.out.print(list.get(i) + " ");
     }
 }
---
+++ Main/카드 역배치.java
@@ -0,0 +1,23 @@
+
+// import java.util.*;
+
+
+// public class Main {
+//     public static void main(String[] arg) {
+//         Scanner in = new Scanner(System.in);
+//         List<Integer> list = new ArrayList<>();
+
+//         for (int i = 1; i < 21; i++)
+//             list.add(i);
+
+//         for (int i = 0; i < 10; i++) {
+//             int s = in.nextInt();
+//             int e = in.nextInt();
+
+//             Collections.reverse(list.subList(s-1, e));
+//         }
+        
+//         for (int i = 0; i < 20; i++) 
+//             System.out.print(list.get(i) + " ");
+//     }
+// }
