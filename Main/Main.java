1885b912244a043e6dce2ed485299ba32d87a764
minseo <kjms0820@gmail.com>
2024-08-14 22:01:17 +0900

    [BOJ] 숫자 / Bronze 2 / 10M / 구현

--- Main/Main.java
+++ Main/Main.java
@@ -5,35 +5,16 @@ import java.util.*;
 public class Main {
     public static void main(String[] arg) {
         Scanner in = new Scanner(System.in);
-        List<Integer> list = new ArrayList<>();
-        int sum = 0;
+        long a = in.nextLong();
+        long b = in.nextLong();
 
-        for (int i = 0; i < 9; i++) {
-            int x = in.nextInt();
-            list.add(x);
-            sum += x;
+        if (a == b) 
+            System.out.println(0);
+        else {
+            System.out.println(Math.max(a, b) - Math.min(a, b) - 1);
+
+            for (long i = Math.min(a, b) + 1; i < Math.max(a, b); i++) 
+                System.out.print(i + " ");
         }
-
-        boolean flag = true;
-
-        for (int i = 0; i < 8; i++) {
-            for (int j = i + 1; j < 9; j++) {
-                if ((list.get(i) + list.get(j)) == (sum - 100)) {
-                    list.set(i, 0);
-                    list.set(j, 0);
-                    flag = false;
-                }
-
-                if (!flag)
-                    break;
-            }
-
-            if (!flag)
-                break;
-        }
-
-        list.sort(Comparator.naturalOrder());
-        for (int i = 2; i < 9; i++)
-            System.out.println(list.get(i));
     }
 }
---
+++ Main/숫자.java
@@ -0,0 +1,19 @@
+// import java.util.*;
+
+
+// public class Main {
+//     public static void main(String[] arg) {
+//         Scanner in = new Scanner(System.in);
+//         long a = in.nextLong();
+//         long b = in.nextLong();
+
+//         if (a == b) 
+//             System.out.println(0);
+//         else {
+//             System.out.println(Math.max(a, b) - Math.min(a, b) - 1);
+
+//             for (long i = Math.min(a, b) + 1; i < Math.max(a, b); i++) 
+//                 System.out.print(i + " ");
+//         }
+//     }
+// }
