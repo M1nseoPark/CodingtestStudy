7f1c7b05ab3192af0518a97b2aa2f3193e54dfc3
minseo <kjms0820@gmail.com>
2024-08-21 22:28:08 +0900

    [BOJ] 별 찍기 - 9 / Bronze 3 / 5M / 구현

---
+++ Main/별 찍기 - 9.java
@@ -0,0 +1,18 @@
+// import java.util.*;
+
+
+// public class Main {
+//     public static void main(String[] arg) {
+//         Scanner in = new Scanner(System.in);
+
+//         int n = in.nextInt();
+//         int x = 2 * n - 1;
+
+//         for (int i = 0; i < n-1; i++) 
+//             System.out.println(" ".repeat(i) + "*".repeat(x-2*i));
+    
+
+//         for (int i = n-1; i >= 0; i--) 
+//             System.out.println(" ".repeat(i) + "*".repeat(x-2*i));
+//     }
+// }
