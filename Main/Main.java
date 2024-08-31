0a11af0db236b92ce107756073b93080b02d1ffd
minseo <kjms0820@gmail.com>
2024-08-31 15:56:48 +0900

    [BOJ] 애너그램 만들기 / Bronze 2 / 10M / 문자열

--- Main/Main.java
+++ Main/Main.java
@@ -5,36 +5,39 @@ import java.util.*;
 public class Main {
     public static void main(String[] arg) {
         Scanner in = new Scanner(System.in);
-        
-        int t = in.nextInt();
 
-        for (int i = 0; i < t; i++) {
-            String a = in.next();
-            String b = in.next();
+        String a = in.nextLine();
+        String b = in.nextLine();
+        int answer = 0;
 
-            if (a.length() != b.length())
-                System.out.println("Impossible");
-            else {
-                HashMap<Character, Integer> amap = new HashMap<>();
-                HashMap<Character, Integer> bmap = new HashMap<>();
+        HashMap<Character, Integer> amap = new HashMap<>();
+        HashMap<Character, Integer> bmap = new HashMap<>();
 
-                for (int j = 0; j < a.length(); j++) {
-                    if (amap.containsKey(a.charAt(j)))
-                        amap.put(a.charAt(j), amap.get(a.charAt(j)) + 1);
-                    else
-                        amap.put(a.charAt(j), 1);
-                    
-                    if (bmap.containsKey(b.charAt(j)))
-                        bmap.put(b.charAt(j), bmap.get(b.charAt(j)) + 1);
-                    else
-                        bmap.put(b.charAt(j), 1);
-                }
-
-                if (amap.equals(bmap)) 
-                    System.out.println("Possible");
-                else
-                    System.out.println("Impossible");
-            }
+        for (int i = 0; i < a.length(); i++) {
+            if (amap.containsKey(a.charAt(i)))
+                amap.put(a.charAt(i), amap.get(a.charAt(i)) + 1);
+            else
+                amap.put(a.charAt(i), 1);
         }
+
+        for (int i = 0; i < b.length(); i++) {
+            if (bmap.containsKey(b.charAt(i)))
+                bmap.put(b.charAt(i), bmap.get(b.charAt(i)) + 1);
+            else
+                bmap.put(b.charAt(i), 1);
+        }
+
+        for (Character c: amap.keySet())
+            if (!bmap.containsKey(c))
+                answer += amap.get(c);
+            else
+                answer += Math.abs(amap.get(c) - bmap.get(c));
+        
+        for (Character c: bmap.keySet())
+            if (!amap.containsKey(c))
+                answer += bmap.get(c);
+
+        
+        System.out.println(answer);
     }
 }
---
+++ Main/애너그램 만들기.java
@@ -0,0 +1,43 @@
+
+// import java.util.*;
+
+
+// public class Main {
+//     public static void main(String[] arg) {
+//         Scanner in = new Scanner(System.in);
+
+//         String a = in.nextLine();
+//         String b = in.nextLine();
+//         int answer = 0;
+
+//         HashMap<Character, Integer> amap = new HashMap<>();
+//         HashMap<Character, Integer> bmap = new HashMap<>();
+
+//         for (int i = 0; i < a.length(); i++) {
+//             if (amap.containsKey(a.charAt(i)))
+//                 amap.put(a.charAt(i), amap.get(a.charAt(i)) + 1);
+//             else
+//                 amap.put(a.charAt(i), 1);
+//         }
+
+//         for (int i = 0; i < b.length(); i++) {
+//             if (bmap.containsKey(b.charAt(i)))
+//                 bmap.put(b.charAt(i), bmap.get(b.charAt(i)) + 1);
+//             else
+//                 bmap.put(b.charAt(i), 1);
+//         }
+
+//         for (Character c: amap.keySet())
+//             if (!bmap.containsKey(c))
+//                 answer += amap.get(c);
+//             else
+//                 answer += Math.abs(amap.get(c) - bmap.get(c));
+        
+//         for (Character c: bmap.keySet())
+//             if (!amap.containsKey(c))
+//                 answer += bmap.get(c);
+
+        
+//         System.out.println(answer);
+//     }
+// }
